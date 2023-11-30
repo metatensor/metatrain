aae14d2640f8df3a5b825e2c6f057b4a1a5f98b5
frostedoyster <bigi.f@libero.it>
2023-11-30 17:21:26 +0100

    Fix data directory in test

--- tests/data.py
+++ tests/data.py
@@ -1,3 +1,4 @@
+import os
 import torch
 
 from metatensor_models.utils.data import Dataset, collate_fn, read_structures, read_targets
@@ -6,8 +7,10 @@ from metatensor_models.utils.data import Dataset, collate_fn, read_structures, r
 def test_dataset():
     """Tests the readers and the dataset class."""
 
-    structures = read_structures("data/qm9_reduced_100.xyz")
-    targets = read_targets("data/qm9_reduced_100.xyz", "U0")
+    dataset_path = os.path.join(os.path.dirname(__file__), "data/qm9_reduced_100.xyz")
+
+    structures = read_structures(dataset_path)
+    targets = read_targets(dataset_path, "U0")
 
     dataset = Dataset(structures, targets)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=10, collate_fn=collate_fn)
