199f98acc91cff196276eea7f6e0ff6c281e2120
frostedoyster <bigi.f@libero.it>
2024-01-31 14:58:47 +0100

    Also fix units in `metatensor.torch.atomistic`

--- scripts/setup.py
+++ scripts/setup.py
@@ -4,6 +4,7 @@
 
 import os
 import metatensor.operations
+import metatensor.torch.atomistic
 
 file = os.path.join(
     os.path.dirname(metatensor.operations.__file__),
@@ -20,6 +21,26 @@ with open(file, "r") as f:
             lines[i] = line.replace(
                 "samples_label = Labels.single()",
                 "samples_label = Labels(names=[\"_\"], values=_dispatch.zeros_like(block.values, (1, 1)))"
+            )
+            break
+
+with open(file, "w") as f:
+    f.writelines(lines)
+
+file = os.path.join(
+    os.path.dirname(metatensor.torch.atomistic.__file__),
+    "units.py"
+)
+
+# At the moment, unit handling is incorrect in metatensor.torch.atomistic
+# This will fix it:
+with open(file, "r") as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if "return self._conversions[to_unit] / self._conversions[from_unit]" in line:
+            lines[i] = line.replace(
+                "return self._conversions[to_unit] / self._conversions[from_unit]",
+                "return self._conversions[from_unit] / self._conversions[to_unit]"
             )
             break
 
