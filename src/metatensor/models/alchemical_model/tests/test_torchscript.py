e14526e4fc68cc83059b78012645920eb3b68592
Arslan Mazitov <arslan.mazitov@phystech.edu>
2024-02-13 19:28:38 +0100

    Removed the metatensor hotfix

--- src/metatensor/models/alchemical_model/tests/test_torchscript.py
+++ src/metatensor/models/alchemical_model/tests/test_torchscript.py
@@ -1,13 +1,3 @@
-import os
-
-
-# Execute the setup script which will make sum_over_samples saveable.
-current_dir = os.path.dirname(__file__)
-setup_path = os.path.join(
-    current_dir, "..", "..", "..", "..", "..", "scripts", "hotfix_metatensor.py"
-)
-exec(open(setup_path).read())
-
 import torch  # noqa: E402
 from metatensor.torch.atomistic import ModelCapabilities, ModelOutput  # noqa: E402
 
