cafdcff81b8f94a2588807988821fb9dce51d3b0
Philip Loche <ploche@physik.fu-berlin.de>
2024-02-19 13:39:43 +0100

    Add ".extxyz" file suffix

--- src/metatensor/models/utils/data/readers/structures/__init__.py
+++ src/metatensor/models/utils/data/readers/structures/__init__.py
@@ -1,4 +1,4 @@
 from .ase import read_structures_ase
 
-STRUCTURE_READERS = {".xyz": read_structures_ase}
+STRUCTURE_READERS = {".extxyz": read_structures_ase, ".xyz": read_structures_ase}
 """:py:class:`dict`: dictionary mapping file suffixes to a structure reader"""
