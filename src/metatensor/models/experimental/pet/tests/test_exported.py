0a84f6d13382a5cd8f1cefd587fb1b26a9225349
frostedoyster <bigi.f@libero.it>
2024-02-29 23:14:56 +0100

    Apply suggestion

--- src/metatensor/models/experimental/pet/tests/test_exported.py
+++ src/metatensor/models/experimental/pet/tests/test_exported.py
@@ -9,25 +9,24 @@ from metatensor.models.utils.model_io import load_exported_model
 
 @pytest.mark.parametrize("device", ["cpu", "cuda"])
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
-def test_to(monkeypatch, tmp_path, device, dtype):
+def test_to(tmp_path, device, dtype):
     """Tests that the `.to()` method of the exported model works."""
     if device == "cuda" and not torch.cuda.is_available():
         pytest.skip("CUDA is not available")
 
-    monkeypatch.chdir(tmp_path)
+    with tmp_path.as_cwd():
+        capabilities = ModelCapabilities(
+            length_unit="Angstrom",
+            species=[1, 6, 7, 8],
+            outputs={
+                "energy": ModelOutput(
+                    quantity="energy",
+                    unit="eV",
+                )
+            },
+        )
+        pet = Model(capabilities, DEFAULT_HYPERS["ARCHITECTURAL_HYPERS"])
+        export(pet, "pet.pt")
+        exported = load_exported_model("pet.pt")
 
-    capabilities = ModelCapabilities(
-        length_unit="Angstrom",
-        species=[1, 6, 7, 8],
-        outputs={
-            "energy": ModelOutput(
-                quantity="energy",
-                unit="eV",
-            )
-        },
-    )
-    pet = Model(capabilities, DEFAULT_HYPERS["ARCHITECTURAL_HYPERS"])
-    export(pet, "pet.pt")
-    exported = load_exported_model("pet.pt")
-
-    exported.to(device=device, dtype=dtype)
+        exported.to(device=device, dtype=dtype)
