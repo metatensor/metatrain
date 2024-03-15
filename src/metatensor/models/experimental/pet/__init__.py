8d59de40fb33dd97b1aafaa8da304a068fedee53
Guillaume Fraux <luthaf@luthaf.fr>
2024-03-15 15:28:07 +0100

    Record authors and maintainers for each architecture (#145)

--- src/metatensor/models/experimental/pet/__init__.py
+++ src/metatensor/models/experimental/pet/__init__.py
@@ -1,2 +1,13 @@
 from .model import Model, DEFAULT_HYPERS  # noqa: F401
 from .train import train  # noqa: F401
+
+__authors__ = [
+    ("Sergey Pozdnyakov <sergey.pozdnyakov@epfl.ch>", "@serfg"),
+    ("Arslan Mazitov <arslan.mazitov@epfl.ch>", "@abmazitov"),
+    ("Filippo Bigi <filippo.bigi@epfl.ch>", "@frostedoyster"),
+]
+
+__maintainers__ = [
+    ("Sergey Pozdnyakov <sergey.pozdnyakov@epfl.ch>", "@serfg"),
+    ("Arslan Mazitov <arslan.mazitov@epfl.ch>", "@abmazitov"),
+]
--- src/metatensor/models/experimental/soap_bpnn/__init__.py
+++ src/metatensor/models/experimental/soap_bpnn/__init__.py
@@ -1,2 +1,10 @@
 from .model import Model, DEFAULT_HYPERS  # noqa: F401
 from .train import train  # noqa: F401
+
+__authors__ = [
+    ("Filippo Bigi <filippo.bigi@epfl.ch>", "@frostedoyster"),
+]
+
+__maintainers__ = [
+    ("Filippo Bigi <filippo.bigi@epfl.ch>", "@frostedoyster"),
+]
