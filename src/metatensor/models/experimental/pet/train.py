c9edaf1b148be1b406de82f88f603d0e1859c157
frostedoyster <bigi.f@libero.it>
2024-03-19 15:06:12 +0100

    Fix merge issue

--- src/metatensor/models/experimental/pet/train.py
+++ src/metatensor/models/experimental/pet/train.py
@@ -10,7 +10,7 @@ from pet.hypers import Hypers
 from pet.pet import PET
 from pet.train_model import fit_pet
 
-from ...utils.data import DatasetInfo, collate_fn, get_all_species
+from ...utils.data import DatasetInfo, check_datasets, collate_fn, get_all_species
 from ...utils.data.system_to_ase import system_to_ase
 from .model import DEFAULT_HYPERS, Model
 
