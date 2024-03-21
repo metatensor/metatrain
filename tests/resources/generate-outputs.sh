bbcf56ea7a8812384b48ea0915f2c7513d298e13
Philip Loche <ploche@physik.fu-berlin.de>
2024-03-21 11:43:50 +0100

    debug tests

--- tests/resources/generate-outputs.sh
+++ tests/resources/generate-outputs.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -e
 
 cd `dirname "$(realpath $0)"`
+
+pwd
 
 metatensor-models train options.yaml -o model-32-bit.pt -y base_precision=32
 metatensor-models train options.yaml -o model-64-bit.pt -y base_precision=64
