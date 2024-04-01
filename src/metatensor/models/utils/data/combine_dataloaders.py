5d09d14f848eedf48eb7751de0c3471ec07903ff
frostedoyster <bigi.f@libero.it>
2024-04-01 20:30:55 +0200

    Fix combined dataloader bug

--- src/metatensor/models/utils/data/combine_dataloaders.py
+++ src/metatensor/models/utils/data/combine_dataloaders.py
@@ -1,57 +1,38 @@
-import itertools
 from typing import List
 
 import numpy as np
 import torch
 
 
-class CombinedIterableDataset(torch.utils.data.IterableDataset):
-    """
-    Combines multiple dataloaders into a single iterable dataset.
-    This is useful for combining multiple dataloaders into a single
-    dataloader. The new dataloader can be shuffled or not.
-
-    :param dataloaders: list of dataloaders to combine
-    :param shuffle: whether to shuffle the combined dataloader
-
-    :return: combined dataloader
-    """
-
-    def __init__(self, dataloaders, shuffle):
+class CombinedDataLoader:
+    def __init__(self, dataloaders: List[torch.utils.data.DataLoader], shuffle: bool):
         self.dataloaders = dataloaders
         self.shuffle = shuffle
 
         # Create the indices:
-        indices = [
-            (i, dl_idx)
-            for dl_idx, dl in enumerate(self.dataloaders)
-            for i in range(len(dl))
-        ]
+        self.indices = list(range(len(self)))
 
         # Shuffle the indices if requested
         if self.shuffle:
-            np.random.shuffle(indices)
+            np.random.shuffle(self.indices)
 
-        self.indices = indices
+        self.reset()
+
+    def reset(self):
+        self.current_index = 0
+        self.full_list = [batch for dl in self.dataloaders for batch in dl]
 
     def __iter__(self):
-        for idx, dataloader_idx in self.indices:
-            yield next(itertools.islice(self.dataloaders[dataloader_idx], idx, None))
+        return self
+
+    def __next__(self):
+        if self.current_index >= len(self.indices):
+            self.reset()  # Reset the index for the next iteration
+            raise StopIteration
+
+        idx = self.indices[self.current_index]
+        self.current_index += 1
+        return self.full_list[idx]
 
     def __len__(self):
-        return len(self.indices)
-
-
-def combine_dataloaders(
-    dataloaders: List[torch.utils.data.DataLoader], shuffle: bool = True
-):
-    """
-    Combines multiple dataloaders into a single dataloader.
-
-    :param dataloaders: list of dataloaders to combine
-    :param shuffle: whether to shuffle the combined dataloader
-
-    :return: combined dataloader
-    """
-    combined_dataset = CombinedIterableDataset(dataloaders, shuffle)
-    return torch.utils.data.DataLoader(combined_dataset, batch_size=None)
+        return sum(len(dl) for dl in self.dataloaders)
