82fe9803d80c3614b52bdf50bd738819fbd74862
frostedoyster <bigi.f@libero.it>
2024-05-16 15:17:59 +0200

    Fix regression tests

--- src/metatensor/models/experimental/soap_bpnn/tests/test_regression.py
+++ src/metatensor/models/experimental/soap_bpnn/tests/test_regression.py
@@ -47,6 +47,9 @@ def test_regression_init():
         [[-0.0564], [0.0296], [0.0182], [-0.1102], [-0.0547]]
     )
 
+    torch.set_printoptions(precision=12)
+    print(output["mtm::U0"].block().values)
+
     torch.testing.assert_close(
         output["mtm::U0"].block().values, expected_output, rtol=1e-3, atol=1e-08
     )
