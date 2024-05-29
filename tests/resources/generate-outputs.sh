fc89719d891f1a6b4402b73578a5f252a4ccb904
Philip Loche <ploche@physik.fu-berlin.de>
2024-05-29 11:32:41 +0200

    forward output

--- tests/resources/generate-outputs.sh
+++ tests/resources/generate-outputs.sh
@@ -7,5 +7,5 @@ ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 cd $ROOT_DIR
 
-metatensor-models --debug train options.yaml -o model-32-bit.pt -r base_precision=32 #> /dev/null
+metatensor-models --debug train options.yaml -o model-32-bit.pt -r base_precision=32 > /dev/null
 metatensor-models train options.yaml -o model-64-bit.pt -r base_precision=64 > /dev/null
