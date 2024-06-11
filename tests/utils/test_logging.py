1058bde18f0d7be8a45807d3dcc122f8f6cb6eee
frostedoyster <bigi.f@libero.it>
2024-06-11 17:40:51 +0200

    Fix test

--- tests/utils/test_logging.py
+++ tests/utils/test_logging.py
@@ -80,7 +80,7 @@ def test_debug_log(caplog, monkeypatch, tmp_path, capsys):
         assert "foo" in logtext
         assert "A debug message" in logtext
         # Test that debug information is in output
-        assert "test_logging.py:test_debug_log:67" in logtext
+        assert "test_logging.py:test_debug_log:68" in logtext
 
 
 def test_metric_logger(caplog, capsys):
