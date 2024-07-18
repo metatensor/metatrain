7931989c4b4764bc38dd4855ff4c99ce6f69347c
frostedoyster <bigi.f@libero.it>
2024-07-18 18:25:15 +0200

    Add test for consistent predictions with different padding

---
+++ src/metatrain/experimental/nanopet/tests/test_nanopet.py
@@ -0,0 +1,50 @@
+from metatrain.experimental.nanopet.model import NanoPET
+import torch
+from metatensor.torch.atomistic import System, ModelOutput
+
+from metatrain.utils.data import DatasetInfo, TargetInfo, TargetInfoDict
+from metatrain.utils.neighbor_lists import get_system_with_neighbor_lists
+
+from . import MODEL_HYPERS
+
+
+
+def test_nanopet_padding():
+    """Tests that the model predicts the same energy independently of the
+    padding size."""
+
+    dataset_info = DatasetInfo(
+        length_unit="Angstrom",
+        atomic_types=[1, 6, 7, 8],
+        targets=TargetInfoDict(energy=TargetInfo(quantity="energy", unit="eV")),
+    )
+
+    model = NanoPET(MODEL_HYPERS, dataset_info)
+
+    system = System(
+        types=torch.tensor([6, 6]),
+        positions=torch.tensor([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]]),
+        cell=torch.zeros(3, 3),
+    )
+    system = get_system_with_neighbor_lists(system, model.requested_neighbor_lists())
+    outputs = {"energy": ModelOutput(per_atom=False)}
+    lone_output = model(
+        [system],
+        outputs
+    )
+
+    system_2 = System(
+        types=torch.tensor([6, 6, 6, 6, 6, 6, 6]),
+        positions=torch.tensor([[0.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 0.0, 2.0], [0.0, 0.0, 3.0], [0.0, 0.0, 4.0], [0.0, 0.0, 5.0], [0.0, 0.0, 6.0]]),
+        cell=torch.zeros(3, 3),
+    )
+    system_2 = get_system_with_neighbor_lists(system_2, model.requested_neighbor_lists())
+    padded_output = model(
+        [system, system_2],
+        outputs
+    )
+
+    lone_energy = lone_output["energy"].block().values.squeeze(-1)[0]
+    padded_energy = padded_output["energy"].block().values.squeeze(-1)[0]
+
+    assert torch.allclose(lone_energy, padded_energy)
