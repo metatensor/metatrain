d27217c3ddf75cc6754cf839a49236fba8a16a4f
Arslan Mazitov <arslan.mazitov@phystech.edu>
2024-10-29 15:50:33 +0100

    Minor fix of the state_dict update

--- src/metatrain/experimental/pet/utils/update_state_dict.py
+++ src/metatrain/experimental/pet/utils/update_state_dict.py
@@ -1,4 +1,7 @@
-def update_state_dict(state_dict: dict) -> dict:
+from typing import Dict
+
+
+def update_state_dict(state_dict: Dict) -> Dict:
     """
     Updates the state_dict keys so they match the model's keys.
     """
@@ -6,4 +9,4 @@ def update_state_dict(state_dict: dict) -> dict:
     for name, value in state_dict.items():
         name = name.split("pet_model.")[1]
         new_state_dict[name] = value
-    return state_dict
+    return new_state_dict
