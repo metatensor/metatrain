0e6c46fcc95cc178fb0c015344cb5c6427656d62
frostedoyster <bigi.f@libero.it>
2024-11-12 15:51:40 +0100

    Fix bug

--- src/metatrain/utils/additive/zbl.py
+++ src/metatrain/utils/additive/zbl.py
@@ -42,7 +42,7 @@ class ZBL(torch.nn.Module):
                     "ZBL only supports energy-like outputs, but a "
                     f"{target.quantity} output was provided."
                 )
-            if not target.layout.is_scalar:
+            if not target.is_scalar:
                 raise ValueError("ZBL only supports scalar outputs")
             if len(target.layout.properties) > 1:
                 raise ValueError(
