59e33639a2eae33d0aa503c676d5b6373e753808
frostedoyster <bigi.f@libero.it>
2024-11-21 20:57:54 +0100

    Turn old warning into error

--- src/metatrain/utils/additive/composition.py
+++ src/metatrain/utils/additive/composition.py
@@ -113,13 +113,10 @@ class CompositionModel(torch.nn.Module):
                     if target_key in get_all_targets(dataset):
                         datasets_with_target.append(dataset)
                 if len(datasets_with_target) == 0:
-                    # this is a possibility when transfer learning
-                    warnings.warn(
+                    raise ValueError(
                         f"Target {target_key} in the model's new capabilities is not "
-                        "present in any of the training datasets.",
-                        stacklevel=2,
+                        "present in any of the training datasets."
                     )
-                    continue
 
                 targets = torch.stack(
                     [
