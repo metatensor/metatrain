1bea8807b9b29e97bb677f607d6dfc1b278bae69
Philip Loche <ploche@physik.fu-berlin.de>
2025-01-31 15:23:33 +0100

    use YYYY.minor version scheme using setuptools-scm

--- src/metatrain/__init__.py
+++ src/metatrain/__init__.py
@@ -1,6 +1,8 @@
 import secrets
 from pathlib import Path
 
+from ._version import __version__  # noqa: F401
+
 
 PACKAGE_ROOT = Path(__file__).parent.resolve()
 
@@ -8,5 +10,3 @@ PACKAGE_ROOT = Path(__file__).parent.resolve()
 # A constant as "session" variable to set the random seed to a fixed value that do not
 # change within the execution of the program.
 RANDOM_SEED = secrets.randbelow(2**32)
-
-__version__ = "2023.11.29"
