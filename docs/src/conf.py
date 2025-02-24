6328ff3c91c8b7d41814c3be79b39a9651ae74d5
frostedoyster <bigi.f@libero.it>
2025-02-24 21:03:07 +0100

    Fix docs?

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -9,6 +9,7 @@ import tomli  # Replace by tomllib from std library once docs are build with Pyt
 # When importing metatensor-torch, this will change the definition of the classes
 # to include the documentation
 os.environ["METATENSOR_IMPORT_FOR_SPHINX"] = "1"
+os.environ["FEATOMIC_IMPORT_FOR_SPHINX"] = "1"
 os.environ["PYTORCH_JIT"] = "0"
 os.environ["METATENSOR_DEBUG_EXTENSIONS_LOADING"] = "1"
 
@@ -54,10 +55,12 @@ def generate_examples():
     # METATENSOR_IMPORT_FOR_SPHINX=1). So instead we run it inside a small script, and
     # include the corresponding output later.
     del os.environ["METATENSOR_IMPORT_FOR_SPHINX"]
+    del os.environ["FEATOMIC_IMPORT_FOR_SPHINX"]
     del os.environ["PYTORCH_JIT"]
     script = os.path.join(ROOT, "docs", "generate_examples", "generate-examples.py")
     subprocess.run([sys.executable, script])
     os.environ["METATENSOR_IMPORT_FOR_SPHINX"] = "1"
+    os.environ["FEATOMIC_IMPORT_FOR_SPHINX"] = "1"
     os.environ["PYTORCH_JIT"] = "0"
 
 
