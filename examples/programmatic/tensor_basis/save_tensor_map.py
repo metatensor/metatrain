8669e2be9be3843412da47d3e99d87f51a46ebb3
frostedoyster <bigi.f@libero.it>
2025-02-25 08:16:00 +0100

    Switch back to `metatensor.torch` to save the `TensorMap`

--- examples/programmatic/tensor_basis/save_tensor_map.py
+++ examples/programmatic/tensor_basis/save_tensor_map.py
@@ -2,17 +2,11 @@ import subprocess
 import sys
 
 import ase.io
-import metatensor as mts
+import metatensor.torch as mts
 import numpy as np
-from featomic.clebsch_gordan import cartesian_to_spherical
+import torch
+from featomic.torch.clebsch_gordan import cartesian_to_spherical
 
-
-# %%
-#
-# In addition to the SOAP-BPNN dependencies, training on a tensor target requires the
-# ``sphericart-torch`` package. To install it, we will run ``pip install`` from this
-# script.
-subprocess.check_call([sys.executable, "-m", "pip", "install", "sphericart-torch"])
 
 # %%
 #
@@ -36,8 +30,8 @@ cartesian_tensormap = mts.TensorMap(
         mts.TensorBlock(
             samples=mts.Labels.range("system", len(molecules)),
             components=[mts.Labels.range(name, 3) for name in ["xyz_1", "xyz_2"]],
-            properties=mts.Labels(["polarizability"], np.array([[0]])),
-            values=polarizabilities[:, :, :, None],
+            properties=mts.Labels(["polarizability"], torch.tensor([[0]])),
+            values=torch.from_numpy(polarizabilities).unsqueeze(-1),
         )
     ],
 )
@@ -45,7 +39,6 @@ cartesian_tensormap = mts.TensorMap(
 # %%
 #
 # Extract from the Cartesian polarizability tensor its irreducible spherical components
-#
 
 spherical_tensormap = mts.remove_dimension(
     cartesian_to_spherical(cartesian_tensormap, components=["xyz_1", "xyz_2"]),
@@ -57,26 +50,10 @@ spherical_tensormap = mts.remove_dimension(
 #
 # We drop the block with ``o3_sigma=-1``, as polarizability should be symmetric and
 # therefore any non-zero pseudo-vector component is spurious.
-#
 spherical_tensormap = mts.drop_blocks(
-    spherical_tensormap, mts.Labels(["o3_sigma"], np.array([[-1]]))
+    spherical_tensormap, mts.Labels(["o3_sigma"], torch.tensor([[-1]]))
 )
 # %%
 #
 # Let's save the spherical components of the polarizability tensor to disk
-#
-# For now, making each array contiguous is necessary for the save function to work
-# (https://github.com/metatensor/metatensor/issues/870)
-blocks = []
-for block in spherical_tensormap.blocks():
-    new_block = mts.TensorBlock(
-        samples=block.samples,
-        components=block.components,
-        properties=block.properties,
-        values=np.ascontiguousarray(block.values),
-    )
-    blocks.append(new_block)
-spherical_tensormap = mts.TensorMap(keys=spherical_tensormap.keys, blocks=blocks)
-
-# save
 mts.save("spherical_polarizability.mts", spherical_tensormap)
