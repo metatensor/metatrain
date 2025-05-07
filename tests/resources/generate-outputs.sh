12f5d70bf58e3de3c64d0636a94b0656eaab555e
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-05-07 14:59:02 +0200

    Make tests faster by reducing hypers and increasing batch_size

--- tests/resources/generate-outputs.sh
+++ tests/resources/generate-outputs.sh
@@ -9,7 +9,7 @@ cd $ROOT_DIR
 
 mtt train options.yaml -o model-32-bit.pt -r base_precision=32 # > /dev/null
 mtt train options.yaml -o model-64-bit.pt -r base_precision=64 # > /dev/null
-mtt train options.yaml -o model-no-extensions.pt -r architecture.name=experimental.nanopet # > /dev/null
+mtt train options-nanopet.yaml -o model-no-extensions.pt # > /dev/null
 
 # upload results to private HF repo if token is set
 if [ -n "${HUGGINGFACE_TOKEN_METATRAIN:-}" ]; then
