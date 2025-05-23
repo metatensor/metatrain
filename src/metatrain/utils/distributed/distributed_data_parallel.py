08061dcc418e30fdb81b15f67bc66bc396dac4ca
frostedoyster <bigi.f@libero.it>
2025-05-23 11:54:41 +0200

    Lint

--- src/metatrain/utils/distributed/distributed_data_parallel.py
+++ src/metatrain/utils/distributed/distributed_data_parallel.py
@@ -7,5 +7,6 @@ class DistributedDataParallel(torch.nn.parallel.DistributedDataParallel):
     :py:class`torch.nn.parallel.DistributedDataParallel`
     and adds a function to retrieve the supported outputs of the module.
     """
+
     def supported_outputs(self):
         return self.module.supported_outputs()
