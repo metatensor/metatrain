202c35488e08cbaf02b7f18942ad9cd3f8a01119
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2025-07-09 17:44:44 +0000

    Fix CompositionModel device bug when running with cuda
    Co-Authored-By: ppegolo <paolo.pegolo@epfl.ch>

--- src/metatrain/utils/additive/composition.py
+++ src/metatrain/utils/additive/composition.py
@@ -9,6 +9,7 @@ from metatomic.torch import ModelOutput, System
 
 from ..data import DatasetInfo, TargetInfo
 from ..jsonschema import validate
+from ..transfer import batch_to
 from ._base_composition import BaseCompositionModel
 from .remove import remove_additive
 
@@ -98,9 +99,11 @@ class CompositionModel(torch.nn.Module):
         if fixed_weights is None:
             fixed_weights = {}
 
+        device = self.dummy_buffer.device
         # accumulate
         for batch in dataloader:
             systems, targets, _ = batch
+            systems, targets, _ = batch_to(systems, targets, device=device)
             # only accumulate the targets that do not use fixed weights
             targets = {
                 target_name: targets[target_name]
@@ -134,7 +137,7 @@ class CompositionModel(torch.nn.Module):
                     mts.make_contiguous(
                         self.model.weights[target_name].to("cpu", torch.float64)
                     )
-                ).to(self.dummy_buffer.device),
+                ).to(device),
             )
 
     def restart(self, dataset_info: DatasetInfo) -> "CompositionModel":
@@ -195,6 +198,11 @@ class CompositionModel(torch.nn.Module):
         :raises ValueError: If no weights have been computed or if `outputs` keys
             contain unsupported keys.
         """
+        dtype = systems[0].positions.dtype
+        device = systems[0].positions.device
+
+        self.weights_to(device, dtype)
+
         for output_name in outputs.keys():
             if output_name not in self.outputs:
                 raise ValueError(
@@ -255,6 +263,8 @@ class CompositionModel(torch.nn.Module):
                 self.model.weights = {
                     k: v.to(dtype) for k, v in self.model.weights.items()
                 }
+
+        self.model._sync_device(device, dtype)
 
     @staticmethod
     def is_valid_target(target_name: str, target_info: TargetInfo) -> bool:
