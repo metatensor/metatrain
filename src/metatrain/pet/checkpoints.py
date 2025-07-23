361207d288c6764eda10a6ac70ea7423e2e30cf3
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-07-23 16:37:52 +0200

    Check if best_model_checkpoint is not None

--- src/metatrain/pet/checkpoints.py
+++ src/metatrain/pet/checkpoints.py
@@ -3,7 +3,10 @@ def update_v1_v2(state_dict):
     # best_model_state_dict and model_state_dict are the same.
     # In that case, the both are updated within the first call of
     # this function in the PET.update_checkpoint() method.
-    if "additive_models.0.model.type_to_index" not in state_dict:
+    if (
+        state_dict is not None
+        and "additive_models.0.model.type_to_index" not in state_dict
+    ):
         state_dict["additive_models.0.model.type_to_index"] = state_dict.pop(
             "additive_models.0.type_to_index"
         )
