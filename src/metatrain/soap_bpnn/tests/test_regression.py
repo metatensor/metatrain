37700041fa70ef369c1a10406efe845d61044786
Joseph W. Abbott <joseph.william.abbott@gmail.com>
2025-08-09 19:03:03 +0200

    Use the new CompositionModel in SOAP-BPNN (#708)

--- src/metatrain/soap_bpnn/tests/test_regression.py
+++ src/metatrain/soap_bpnn/tests/test_regression.py
@@ -1,12 +1,13 @@
 import random
 
 import numpy as np
+import pytest
 import torch
 from metatomic.torch import ModelOutput
 from omegaconf import OmegaConf
 
 from metatrain.soap_bpnn import SoapBpnn, Trainer
-from metatrain.utils.data import Dataset, DatasetInfo
+from metatrain.utils.data import Dataset, DatasetInfo, get_dataset
 from metatrain.utils.data.readers import read_systems, read_targets
 from metatrain.utils.data.target_info import get_energy_target_info
 from metatrain.utils.neighbor_lists import (
@@ -14,7 +15,7 @@ from metatrain.utils.neighbor_lists import (
     get_system_with_neighbor_lists,
 )
 
-from . import DATASET_PATH, DEFAULT_HYPERS, MODEL_HYPERS
+from . import DATASET_PATH, DEFAULT_HYPERS, MODEL_HYPERS, SPHERICAL_DISK_DATASET_PATH
 
 
 def test_regression_init():
@@ -62,8 +63,11 @@ def test_regression_init():
     torch.testing.assert_close(output["mtt::U0"].block().values, expected_output)
 
 
-def test_regression_train():
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+def test_regression_train(device):
     """Regression test for the model when trained for 2 epoch on a small dataset"""
+    if device == "cuda" and not torch.cuda.is_available():
+        pytest.skip("CUDA is not available")
     random.seed(0)
     np.random.seed(0)
     torch.manual_seed(0)
@@ -102,7 +106,7 @@ def test_regression_train():
     trainer.train(
         model=model,
         dtype=torch.float32,
-        devices=[torch.device("cpu")],
+        devices=[torch.device(device)],
         train_datasets=[dataset],
         val_datasets=[dataset],
         checkpoint_dir=".",
@@ -121,11 +125,11 @@ def test_regression_train():
 
     expected_output = torch.tensor(
         [
-            [1.643912792206],
-            [1.634970188141],
-            [5.043813705444],
-            [10.300852775574],
-            [2.965628623962],
+            [1.313831329346],
+            [4.282802581787],
+            [5.629218101501],
+            [4.297019958496],
+            [2.226531982422],
         ]
     )
 
@@ -134,3 +138,117 @@ def test_regression_train():
     # print(output["mtt::U0"].block().values)
 
     torch.testing.assert_close(output["mtt::U0"].block().values, expected_output)
+
+
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+def test_regression_train_spherical(device):
+    """Regression test for the model when trained for 2 epoch on a small dataset"""
+    if device == "cuda" and not torch.cuda.is_available():
+        pytest.skip("CUDA is not available")
+    random.seed(0)
+    np.random.seed(0)
+    torch.manual_seed(0)
+
+    conf = {
+        "systems": {"read_from": SPHERICAL_DISK_DATASET_PATH},
+        "targets": {
+            "mtt::electron_density_basis": {
+                "quantity": "",
+                "unit": "",
+                "read_from": SPHERICAL_DISK_DATASET_PATH,
+                "type": {
+                    "spherical": {
+                        "irreps": [
+                            {"o3_lambda": 0, "o3_sigma": 1},
+                            {"o3_lambda": 1, "o3_sigma": 1},
+                            {"o3_lambda": 2, "o3_sigma": 1},
+                            {"o3_lambda": 3, "o3_sigma": 1},
+                        ]
+                    },
+                },
+                "per_atom": True,
+                "num_subtargets": 1,  # dummy value
+            },
+        },
+    }
+    # targets, target_info_dict = read_targets(OmegaConf.create(conf))
+    # dataset = Dataset.from_dict({"system": systems, "mtt::U0": targets["mtt::U0"]})
+
+    dataset, target_info_dict, _ = get_dataset(conf)
+
+    hypers = DEFAULT_HYPERS.copy()
+    hypers["training"]["num_epochs"] = 2
+    hypers["training"]["batch_size"] = 1
+
+    dataset_info = DatasetInfo(
+        length_unit="Angstrom", atomic_types=[1, 6, 7, 8], targets=target_info_dict
+    )
+    model = SoapBpnn(MODEL_HYPERS, dataset_info)
+    requested_neighbor_lists = get_requested_neighbor_lists(model)
+
+    hypers["training"]["num_epochs"] = 1
+    trainer = Trainer(hypers["training"])
+    trainer.train(
+        model=model,
+        dtype=torch.float32,
+        devices=[torch.device(device)],
+        train_datasets=[dataset],
+        val_datasets=[dataset],
+        checkpoint_dir=".",
+    )
+
+    # Predict on the first five systems
+    systems = [sample["system"] for sample in dataset]
+    systems = [system.to(torch.float32) for system in systems]
+    systems = [
+        get_system_with_neighbor_lists(system, requested_neighbor_lists)
+        for system in systems
+    ]
+    output = model(
+        systems,
+        {
+            "mtt::electron_density_basis": ModelOutput(
+                quantity="energy", unit="", per_atom=True
+            )
+        },
+    )
+
+    expected_output = torch.tensor(
+        [
+            [
+                -0.038565825671,
+                0.000463733566,
+                0.000264365954,
+                0.023815866560,
+                0.018959790468,
+                -0.000692606962,
+                0.020604602993,
+            ],
+            [
+                0.000000000000,
+                0.000000000000,
+                0.000000000000,
+                0.000000000000,
+                0.000000000000,
+                0.000000000000,
+                0.000000000000,
+            ],
+            [
+                0.024628046900,
+                -0.001363838091,
+                0.003145742230,
+                -0.024710856378,
+                -0.010125328787,
+                -0.015510082245,
+                -0.014338681474,
+            ],
+        ],
+    )
+
+    # if you need to change the hardcoded values:
+    torch.set_printoptions(precision=12)
+    print(output["mtt::electron_density_basis"][1].values[2])
+
+    torch.testing.assert_close(
+        output["mtt::electron_density_basis"][1].values[2], expected_output
+    )
