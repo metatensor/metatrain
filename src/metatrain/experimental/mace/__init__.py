7f7226c2a93999ec787276d1741260e4125fa8ee
Pol <pfebrer96@gmail.com>
2025-08-13 23:30:01 +0200

    Add MACE model

---
+++ src/metatrain/experimental/mace/__init__.py
@@ -0,0 +1,14 @@
+from .model import MetaMACE
+from .trainer import Trainer
+
+
+__model__ = MetaMACE
+__trainer__ = Trainer
+
+__authors__ = [
+    ("Pol Febrer <pol.febrer@epfl.ch>", "@pfebrer"),
+]
+
+__maintainers__ = [
+    ("Pol Febrer <pol.febrer@epfl.ch>", "@pfebrer"),
+]
