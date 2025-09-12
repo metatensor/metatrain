4c91d697da8c67c8bbc0649d7877e8ba24e48c53
frostedoyster <bigi.f@libero.it>
2025-09-12 10:26:33 +0200

    Fix stuff

--- src/metatrain/llpr/checkpoints.py
+++ src/metatrain/llpr/checkpoints.py
@@ -1,11 +1,10 @@
 def model_update_v1_v2(checkpoint):
-    print(checkpoint.keys())
     # Here we need to add model_data to the old checkpoint:
     checkpoint["model_data"] = {}
     checkpoint["model_data"]["hypers"] = {
         "ensembles": {
             "num_members": {"energy": 128},
-            "means": {"energy": []},
+            "means": {"energy": []},  # not needed to load the state_dict correctly
         },
     }
     checkpoint["model_data"]["dataset_info"] = checkpoint["wrapped_model_checkpoint"][
