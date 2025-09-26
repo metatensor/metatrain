c49063f0556297f0ebd815fbc4a8329419c68561
frostedoyster <bigi.f@libero.it>
2025-09-26 20:06:03 +0200

    Fix tests

--- tests/utils/test_architectures.py
+++ tests/utils/test_architectures.py
@@ -22,7 +22,7 @@ def is_None(*args, **kwargs) -> None:
 
 def test_find_all_architectures():
     all_arches = find_all_architectures()
-    assert len(all_arches) == 6
+    assert len(all_arches) == 7
 
     assert "gap" in all_arches
     assert "pet" in all_arches
