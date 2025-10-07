af2e7d831667f8e3d55d76f093078f09359393fc
HowWeiBin <kolking6@gmail.com>
2025-10-07 13:59:32 +0200

    Add Squeeze

--- src/metatrain/pet/trainer.py
+++ src/metatrain/pet/trainer.py
@@ -417,7 +417,7 @@ class Trainer:
                 gap_force_predictions = torch.vstack(compute_gradient(bandgap_predictions, [system.positions for system in systems], is_training=True))
                 bandgap_target = gap_batch[0].values.reshape(-1,1).to(dtype)
                 bandgap_loss = torch.nn.functional.mse_loss(bandgap_predictions, bandgap_target)
-                gap_force_target = gap_force_batch[0].values.to(dtype)
+                gap_force_target = torch.squeeze(gap_force_batch[0].values.to(dtype))
                 print (gap_force_target.shape)
                 print (gap_force_predictions.shape)
                 gap_force_loss = torch.nn.functional.mse_loss(gap_force_predictions, gap_force_target) # Use 1:1 for now
