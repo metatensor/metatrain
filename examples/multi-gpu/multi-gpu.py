e5724a1fcefd7b2f8a72b7bf0f54d59039350a58
GardevoirX <xqj.2001@outlook.com>
2025-10-08 17:22:57 +0200

    Remove the slurm script

--- examples/multi-gpu/multi-gpu.py
+++ examples/multi-gpu/multi-gpu.py
@@ -24,8 +24,21 @@ for the training. Let's take `this tutorial
 Below is the ``slurm`` script for submitting the job. ``metatrain`` will automatically
 use all the GPUs that you have asked for.
 
-.. literalinclude:: ./submit.sh
-   :language: bash
+.. code-block:: bash
+
+    #!/bin/bash
+    #SBATCH --nodes 1
+    #SBATCH --ntasks 2  # must equal to the number of GPUs
+    #SBATCH --ntasks-per-node 2
+    #SBATCH --gpus-per-node 2  # use 2 GPUs
+    #SBATCH --cpus-per-task 8
+    #SBATCH --exclusive
+    #SBATCH --partition=h100  # adapt this to your cluster
+    #SBATCH --time=1:00:00
+
+    # load modules and/or virtual environments and/or containers here
+
+    srun mtt train options-distributed.yaml
 
 Performance
 -----------
