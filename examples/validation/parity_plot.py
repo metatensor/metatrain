d8c998aefa34eeecab3cb9c1c51e3595c68d1509
GardevoirX <xqj.2001@outlook.com>
2025-10-13 15:33:19 +0200

    Linter!

--- examples/validation/parity_plot.py
+++ examples/validation/parity_plot.py
@@ -83,12 +83,13 @@ print("RMSE forces:", np.sqrt(np.mean((f_targets - f_predictions) ** 2)), "kcal/
 # was created only for demonstration purposes. In the case of a well-trained model, the
 # points should be closer to the diagonal line.
 
-# %%
-# Check outliers with ``Chemiscope``
+# %% Check outliers with ``Chemiscope``
 # ----------------------------------
-# With the approach above, you can inspect the whole dataset, but it might be difficult to identify outliers. `Chemiscope <https://chemiscope.org/docs/index.html>` is a visualisation tool,
-# allowing you to explore the dataset interactively. The following example shows how to
-# use it to check the structure of probable outliers and the atomic forces.
+# With the approach above, you can inspect the whole dataset, but it might be difficult
+# to identify outliers. `Chemiscope <https://chemiscope.org/docs/index.html>` is a
+# visualisation tool, allowing you to explore the dataset interactively. The following
+# example shows how to use it to check the structure of probable outliers and the atomic
+# forces.
 
 for frame in targets + predictions:
     frame.arrays["forces"] = frame.get_forces()
