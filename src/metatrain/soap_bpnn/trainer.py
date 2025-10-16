162f0a510318bce0c441a769c3df08c8c3235963
HowWeiBin <kolking6@gmail.com>
2025-10-16 11:06:10 +0200

    Merge2

--- src/metatrain/soap_bpnn/trainer.py
+++ src/metatrain/soap_bpnn/trainer.py
@@ -131,6 +131,7 @@ class Trainer(TrainerInterface):
         # numerical errors in the composition weights, which can be very large).
         for additive_model in model.additive_models:
             additive_model.to(dtype=torch.float64)
+        model.scaler.to(dtype=torch.float64)
         if self.hypers["fit_composition_model"]:
             logging.info("Calculating composition weights")
             model.additive_models[0].train_model(  # this is the composition model
