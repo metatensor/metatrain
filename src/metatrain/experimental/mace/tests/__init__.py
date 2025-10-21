c2e0373f3f8ea453a8b5ca120810f659eb5f7920
Joseph Abbott <joseph.william.abbott@gmail.com>
2025-10-21 11:22:19 +0200

    hypers

--- src/metatrain/experimental/mace/tests/__init__.py
+++ src/metatrain/experimental/mace/tests/__init__.py
@@ -8,5 +8,5 @@ DATASET_WITH_FORCES_PATH = str(
     Path(__file__).parents[4] / "tests/resources/carbon_reduced_100.xyz"
 )
 
-DEFAULT_HYPERS = get_default_hypers("pet")
+DEFAULT_HYPERS = get_default_hypers("mace")
 MODEL_HYPERS = DEFAULT_HYPERS["model"]
