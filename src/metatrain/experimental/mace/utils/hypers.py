3d612a1451d934bc8a3a3abbe359c2c8ed038d4f
Pol <pfebrer96@gmail.com>
2025-10-22 12:22:33 +0200

    Moved hypers generation to a _dev folder

--- src/metatrain/experimental/mace/utils/hypers.py
+++ src/metatrain/experimental/mace/utils/hypers.py
@@ -1,17 +1,10 @@
-"""
-Fills the default_hypers_template.yaml with the default arguments
-from MACE.
-"""
-
 import argparse
-from pathlib import Path
 
-from jinja2 import Environment, FileSystemLoader
 from mace.tools import build_default_arg_parser
 
 
 # Keys from the MACE argparser that correspond to model hyperparameters
-mace_model_keys = [
+MACE_MODEL_ARG_KEYS = [
     "num_radial_basis",
     "num_cutoff_basis",
     "max_ell",
@@ -50,29 +43,3 @@ def get_mace_defaults():
     mace_defaults["gate"] = None
 
     return mace_defaults
-
-
-def regenerate_default_hypers():
-    # Extract defaults without triggering required arguments
-    mace_defaults = get_mace_defaults()
-
-    # Render jinja2 template
-    env = Environment(
-        loader=FileSystemLoader(Path(__file__).parent),
-        trim_blocks=True,
-        lstrip_blocks=True,
-    )
-    template = env.get_template("default-hypers-template.yaml")
-
-    mace_model_defaults = [(k, mace_defaults[k]) for k in mace_model_keys]
-
-    with open(Path(__file__).parent / "default-hypers.temp_yaml", "w") as f:
-        f.write(
-            template.render(
-                mace_defaults=mace_defaults, mace_model_defaults=mace_model_defaults
-            )
-        )
-
-
-if __name__ == "__main__":
-    regenerate_default_hypers()
