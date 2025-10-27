2d5ee20dd76ca9dcb7a12cdc92d118aeffdb6ef2
E-Rum <rev99@internet.ru>
2025-10-27 11:55:44 +0000

    Add test for long-range module handling of 1D periodic structures

--- tests/utils/test_long_range.py
+++ tests/utils/test_long_range.py
@@ -57,3 +57,36 @@ def test_long_range(periodicity, architecture_name, tmpdir):
         torch.jit.save(model, "model.pt")
         model = torch.jit.load("model.pt")
         model(systems, {"energy": model.outputs["energy"]})
+
+
+@pytest.mark.parametrize("architecture_name", ["pet", "soap_bpnn"])
+def test_long_range_1d_structure(architecture_name):
+    """Tests that the long-range module can't handle 1D periodic structures."""
+    structures = read(RESOURCES_PATH / "carbon_reduced_100.xyz", ":1")
+    structures[0].pbc = [True, False, False]
+    structures[0].cell = [[10.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]
+    systems = systems_to_torch(structures)
+
+    dataset_info = DatasetInfo(
+        length_unit="Angstrom",
+        atomic_types=[1, 6, 8],
+        targets={"energy": get_energy_target_info("energy", {"unit": "eV"})},
+    )
+
+    hypers = get_default_hypers(architecture_name)
+    hypers["model"]["long_range"]["enable"] = True
+    hypers["model"]["long_range"]["use_ewald"] = True
+
+    architecture = import_architecture(architecture_name)
+    Model = architecture.__model__
+    model = Model(hypers["model"], dataset_info)
+    requested_nls = get_requested_neighbor_lists(model)
+
+    systems = [
+        get_system_with_neighbor_lists(system, requested_nls) for system in systems
+    ]
+
+    with pytest.raises(
+        NotImplementedError, match="Long-range featurizer does not support 1D systems."
+    ):
+        model(systems, {"energy": model.outputs["energy"]})
