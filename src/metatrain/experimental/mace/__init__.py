6dea28249f5a093ed902a01763df3bd24b94a07f
Pol <pfebrer96@gmail.com>
2025-10-29 15:50:42 +0100

    Move to e3nn's compiling

--- src/metatrain/experimental/mace/__init__.py
+++ src/metatrain/experimental/mace/__init__.py
@@ -1,10 +1,6 @@
 from .model import MetaMACE
 from .trainer import Trainer
-from .utils.e3nn import patch_e3nn
 
-
-# Patch e3nn to make it torchscript compatible
-patch_e3nn()
 
 __model__ = MetaMACE
 __trainer__ = Trainer
