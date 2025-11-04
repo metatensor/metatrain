f9d82c98771b50fe9bf30bc53c5cd2520f0bea51
HowWeiBin <kolking6@gmail.com>
2025-11-04 07:57:39 +0100

    Debug attempt 3

--- src/metatrain/pet/trainer.py
+++ src/metatrain/pet/trainer.py
@@ -389,7 +389,8 @@ class Trainer(TrainerInterface):
                 systems, targets, extra_data = batch_to(
                     systems, targets, extra_data, dtype=dtype, device=device
                 )
-
+                print ("number of systems")
+                print (len(systems))
                 new_systems = []
                 for system in systems:
                     new_system, strain = _prepare_system(
@@ -400,7 +401,8 @@ class Trainer(TrainerInterface):
                     )
                 new_systems.append(new_system)
                 systems = new_systems
-
+                print ("Final")
+                print (len(systems))
                 predictions = evaluate_model(
                     model,
                     systems,
@@ -467,8 +469,6 @@ class Trainer(TrainerInterface):
                 systems, targets, extra_data = batch_to(
                     systems, targets, extra_data, dtype=dtype, device=device
                 )
-                print ("number of systems")
-                print (len(systems))
                 new_systems = []
                 for system in systems:
                     new_system, strain = _prepare_system(
@@ -479,8 +479,6 @@ class Trainer(TrainerInterface):
                     )
                 new_systems.append(new_system)
                 systems = new_systems
-                print ("Final")
-                print (len(systems))
                 predictions = evaluate_model(
                     model,
                     systems,
