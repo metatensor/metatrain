Implement scaling for all blocks. Fix CompositionModel masking issue. Fix Hermitian inversion parity. Fix eval with masks.

src/metatrain/cli/eval.py
@@ -17,7 +17,9 @@ from metatrain.utils.data import (
     CollateFn,
     Dataset,
     TargetInfo,
+    get_create_dynamic_target_mask_transform,
     get_dataset,
+    get_pad_samples_transform,
     read_systems,
     unpack_batch,
 )
@@ -172,7 +174,16 @@ def _eval_targets(
     requested_neighbor_lists = get_requested_neighbor_lists(model)
     collate_fn = CollateFn(
         target_keys,
-        callables=[get_system_with_neighbor_lists_transform(requested_neighbor_lists)],
+        callables=[
+            get_system_with_neighbor_lists_transform(requested_neighbor_lists),
+            get_pad_samples_transform(
+                target_keys,  # TODO: for spherical per-atom only
+                requested_neighbor_lists[0],
+            ),
+            get_create_dynamic_target_mask_transform(
+                target_keys,  # TODO: for spherical per-atom only
+            ),
+        ],
     )
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=False

src/metatrain/pet/tests/test_matrix.py
@@ -21,14 +21,18 @@ from . import (
 
 def get_frames():
     """
-    Returns 3 physically equivalent CH4 systems with different atom orderings.
+    Returns 3 physically equivalent C2H3 systems with different atom orderings.
 
-    The second is the same as the first but with atoms 3 (H) and 4 (H) swapped.
-    The third is the same as the first but with atoms 1 (C) and 2 (H) swapped.
+    This is based on the geometry of a methane molecule, but with a C-for-H
+    substitution. Having a C-C interaction is important for this test due to the
+    inversion parity under transpose of p-p interactions.
+
+    The second is the same as the first but with atoms 3 (H) and 4 (H) swapped. The
+    third is the same as the first but with atoms 1 (C) and 2 (H) swapped.
     """
     return [
         ase.Atoms(
-            numbers=[6, 1, 1, 1, 1],
+            numbers=[6, 1, 6, 1, 1],
             positions=[
                 [0.99826, -0.00246, -0.00436],
                 [2.09021, -0.00243, 0.00414],
@@ -38,7 +42,7 @@ def get_frames():
             ],
         ),
         ase.Atoms(
-            numbers=[6, 1, 1, 1, 1],
+            numbers=[6, 1, 6, 1, 1],
             positions=[
                 [0.99826, -0.00246, -0.00436],
                 [2.09021, -0.00243, 0.00414],
@@ -48,7 +52,7 @@ def get_frames():
             ],
         ),
         ase.Atoms(
-            numbers=[1, 6, 1, 1, 1],
+            numbers=[1, 6, 6, 1, 1],
             positions=[
                 [2.09021, -0.00243, 0.00414],  # swapped
                 [0.99826, -0.00246, -0.00436],  # swapped
@@ -103,8 +107,7 @@ def test_matrix_hermitian(dtype):
         tensor=output,
     )
     for mat in output_matrices:
-        assert torch.allclose(mat[(0, 0, 0)], mat[(0, 0, 0)].conj().T)
-        print((mat[(0, 0, 0)] - mat[(0, 0, 0)].conj().T).abs().mean())
+        assert torch.allclose(mat["0_0_0"], mat["0_0_0"].conj().T)
 
 
 @pytest.mark.parametrize("dtype", [torch.float32, torch.float64])
@@ -151,21 +154,19 @@ def test_matrix_atom_swapping(dtype):
     )
 
     # Check H H permutation
-    mat1 = output_matrices[0][0, 0, 0].detach().numpy()
+    mat1 = output_matrices[0]["0_0_0"].detach().numpy()
     mat2 = _permute_atomic_subblocks(
-        output_matrices[1][0, 0, 0].detach().numpy(),
+        output_matrices[1]["0_0_0"].detach().numpy(),
         frames[1],
         basis_set,
         [0, 1, 2, 4, 3],
     )
     mat3 = _permute_atomic_subblocks(
-        output_matrices[2][0, 0, 0].detach().numpy(),
+        output_matrices[2]["0_0_0"].detach().numpy(),
         frames[2],
         basis_set,
         [1, 0, 2, 3, 4],
     )
-    print("H H swap diff mean:", np.abs(mat1 - mat2).mean())
-    print("C H swap diff mean:", np.abs(mat1 - mat3).mean())
 
     if dtype == torch.float32:
         rtol = 1e-5

src/metatrain/pet/trainer.py
@@ -158,26 +158,10 @@ class Trainer(TrainerInterface):
             additive_model.to(dtype=torch.float64)
         model.scaler.to(dtype=torch.float64)
 
-        # Extract additive models and scaler and move them to CPU/float64 so they
-        # can be used in the collate function
-        model.additive_models[0].weights_to(device="cpu", dtype=torch.float64)
-        additive_models = copy.deepcopy(
-            model.additive_models.to(dtype=torch.float64, device="cpu")
-        )
-        model.additive_models.to(device)
-        model.additive_models[0].weights_to(device=device, dtype=torch.float64)
-        model.scaler.scales_to(device="cpu", dtype=torch.float64)
-        scaler = copy.deepcopy(model.scaler.to(dtype=torch.float64, device="cpu"))
-        model.scaler.to(device)
-        model.scaler.scales_to(device=device, dtype=torch.float64)
-
-        # Create collate functions:
+        # Train composition model and scaler
         dataset_info = model.dataset_info
         train_targets = dataset_info.targets
         extra_data_info = dataset_info.extra_data
-        rotational_augmenter = RotationalAugmenter(
-            target_info_dict=train_targets, extra_data_info_dict=extra_data_info
-        )
         requested_neighbor_lists = get_requested_neighbor_lists(model)
         spherical_per_atom_targets = [
             name
@@ -189,112 +173,44 @@ class Trainer(TrainerInterface):
             for name, hypers in self.hypers["loss"].items()
             if "masked_" in hypers["type"]
         ]
-        collate_fn_train = CollateFn(
-            target_keys=list(train_targets.keys()),
-            callables=[
-                get_reindex_system_to_batch_id_transform(),
-                get_system_with_neighbor_lists_transform(requested_neighbor_lists),
-                get_pad_samples_transform(
-                    spherical_per_atom_targets,
-                    requested_neighbor_lists[0],
-                ),
-                rotational_augmenter.apply_random_augmentations,
-                get_create_dynamic_target_mask_transform(dynamic_mask_targets),
-                get_remove_additive_transform(additive_models, train_targets),
-                get_remove_scale_transform(scaler),
-            ],
-        )
-        collate_fn_val = CollateFn(
-            target_keys=list(train_targets.keys()),
-            callables=[  # no augmentation for validation
-                get_reindex_system_to_batch_id_transform(),
-                get_system_with_neighbor_lists_transform(requested_neighbor_lists),
-                get_pad_samples_transform(
-                    spherical_per_atom_targets,
-                    requested_neighbor_lists[0],
-                ),
-                get_create_dynamic_target_mask_transform(dynamic_mask_targets),
-                get_remove_additive_transform(additive_models, train_targets),
-                get_remove_scale_transform(scaler),
-            ],
-        )
 
-        # Extract additive models and scaler and move them to CPU/float64 so they
-        # can be used in the collate function
-        model.additive_models[0].weights_to(device="cpu", dtype=torch.float64)
-        additive_models = copy.deepcopy(
-            model.additive_models.to(dtype=torch.float64, device="cpu")
+        # if self.hypers["remove_composition_contribution"]:
+        logging.info("Calculating composition weights")
+        model.additive_models[0].train_model(  # this is the composition model
+            train_datasets,
+            model.additive_models[1:],
+            CollateFn(
+                target_keys=list(train_targets.keys()),
+                callables=[
+                    get_reindex_system_to_batch_id_transform(),
+                    get_system_with_neighbor_lists_transform(requested_neighbor_lists),
+                    get_create_dynamic_target_mask_transform(dynamic_mask_targets),
+                ],
+            ),
+            self.hypers["batch_size"],
+            is_distributed,
+            self.hypers["fixed_composition_weights"],
         )
-        model.additive_models.to(device)
-        model.additive_models[0].weights_to(device=device, dtype=torch.float64)
-        model.scaler.scales_to(device="cpu", dtype=torch.float64)
-        scaler = copy.deepcopy(model.scaler.to(dtype=torch.float64, device="cpu"))
-        model.scaler.to(device)
-        model.scaler.scales_to(device=device, dtype=torch.float64)
-
-        # Create collate functions:
-        dataset_info = model.dataset_info
-        train_targets = dataset_info.targets
-        extra_data_info = dataset_info.extra_data
-        rotational_augmenter = RotationalAugmenter(
-            target_info_dict=train_targets, extra_data_info_dict=extra_data_info
-        )
-        requested_neighbor_lists = get_requested_neighbor_lists(model)
-        spherical_per_atom_targets = [
-            name
-            for name, target in train_targets.items()
-            if target.is_spherical and target.per_atom
-        ]
-        dynamic_mask_targets = [
-            name
-            for name, hypers in self.hypers["loss"].items()
-            if "masked_" in hypers["type"]
-        ]
-        collate_fn_train = CollateFn(
-            target_keys=list(train_targets.keys()),
-            callables=[
-                get_reindex_system_to_batch_id_transform(),
-                get_system_with_neighbor_lists_transform(requested_neighbor_lists),
-                get_pad_samples_transform(
-                    spherical_per_atom_targets,
-                    requested_neighbor_lists[0],
-                ),
-                rotational_augmenter.apply_random_augmentations,
-                get_create_dynamic_target_mask_transform(dynamic_mask_targets),
-                get_remove_additive_transform(additive_models, train_targets),
-                get_remove_scale_transform(scaler),
-            ],
-        )
-        collate_fn_val = CollateFn(
-            target_keys=list(train_targets.keys()),
-            callables=[  # no augmentation for validation
-                get_reindex_system_to_batch_id_transform(),
-                get_system_with_neighbor_lists_transform(requested_neighbor_lists),
-                get_pad_samples_transform(
-                    spherical_per_atom_targets,
-                    requested_neighbor_lists[0],
-                ),
-                get_create_dynamic_target_mask_transform(dynamic_mask_targets),
-                get_remove_additive_transform(additive_models, train_targets),
-                get_remove_scale_transform(scaler),
-            ],
-        )
-
-        if self.hypers["remove_composition_contribution"]:
-            logging.info("Calculating composition weights")
-            model.additive_models[0].train_model(  # this is the composition model
-                train_datasets,
-                model.additive_models[1:],
-                self.hypers["batch_size"],
-                is_distributed,
-                self.hypers["fixed_composition_weights"],
-            )
 
         if self.hypers["scale_targets"]:
             logging.info("Calculating scaling weights")
             model.scaler.train_model(
                 train_datasets,
-                collate_fn_val,  # we don't need the augmenter here
+                model.additive_models,
+                CollateFn(
+                    target_keys=list(train_targets.keys()),
+                    callables=[
+                        get_reindex_system_to_batch_id_transform(),
+                        get_system_with_neighbor_lists_transform(
+                            requested_neighbor_lists
+                        ),
+                        get_pad_samples_transform(
+                            spherical_per_atom_targets,
+                            requested_neighbor_lists[0],
+                        ),
+                        get_create_dynamic_target_mask_transform(dynamic_mask_targets),
+                    ],
+                ),
                 self.hypers["batch_size"],
                 is_distributed,
                 self.hypers["fixed_scaling_weights"],
@@ -327,6 +243,53 @@ class Trainer(TrainerInterface):
             train_samplers = [None] * len(train_datasets)
             val_samplers = [None] * len(val_datasets)
 
+        # Extract additive models and scaler and move them to CPU/float64 so they
+        # can be used in the collate function
+        model.additive_models[0].weights_to(device="cpu", dtype=torch.float64)
+        additive_models = copy.deepcopy(
+            model.additive_models.to(dtype=torch.float64, device="cpu")
+        )
+        model.additive_models.to(device)
+        model.additive_models[0].weights_to(device=device, dtype=torch.float64)
+        model.scaler.scales_to(device="cpu", dtype=torch.float64)
+        scaler = copy.deepcopy(model.scaler.to(dtype=torch.float64, device="cpu"))
+        model.scaler.to(device)
+        model.scaler.scales_to(device=device, dtype=torch.float64)
+
+        # Create collate functions:
+        rotational_augmenter = RotationalAugmenter(
+            target_info_dict=train_targets, extra_data_info_dict=extra_data_info
+        )
+        collate_fn_train = CollateFn(
+            target_keys=list(train_targets.keys()),
+            callables=[
+                get_reindex_system_to_batch_id_transform(),
+                get_system_with_neighbor_lists_transform(requested_neighbor_lists),
+                get_pad_samples_transform(
+                    spherical_per_atom_targets,
+                    requested_neighbor_lists[0],
+                ),
+                # rotational_augmenter.apply_random_augmentations,
+                get_create_dynamic_target_mask_transform(dynamic_mask_targets),
+                get_remove_additive_transform(additive_models, train_targets),
+                get_remove_scale_transform(scaler),
+            ],
+        )
+        collate_fn_val = CollateFn(
+            target_keys=list(train_targets.keys()),
+            callables=[  # no augmentation for validation
+                get_reindex_system_to_batch_id_transform(),
+                get_system_with_neighbor_lists_transform(requested_neighbor_lists),
+                get_pad_samples_transform(
+                    spherical_per_atom_targets,
+                    requested_neighbor_lists[0],
+                ),
+                get_create_dynamic_target_mask_transform(dynamic_mask_targets),
+                get_remove_additive_transform(additive_models, train_targets),
+                get_remove_scale_transform(scaler),
+            ],
+        )
+
         # Create dataloader for the training datasets:
         if self.hypers["num_workers"] is None:
             num_workers = get_num_workers()
@@ -356,13 +319,11 @@ class Trainer(TrainerInterface):
                     sampler=train_sampler,
                     shuffle=(
                         # the sampler takes care of this (if present)
-                        train_sampler
-                        is None
+                        train_sampler is None
                     ),
                     drop_last=(
                         # the sampler takes care of this (if present)
-                        train_sampler
-                        is None
+                        train_sampler is None
                     ),
                     collate_fn=collate_fn_train,
                     num_workers=num_workers,

src/metatrain/utils/additive/_base_composition.py
@@ -178,17 +178,18 @@ class BaseCompositionModel(torch.nn.Module):
             blocks=[
                 TensorBlock(
                     values=torch.zeros(
-                        len(self.atomic_types),
-                        len(self.atomic_types),
+                        len(self.atomic_types) ** 2,
+                        len(block.properties),
                         dtype=torch.float64,
                     ),
-                    samples=Labels(["center_type"], self.atomic_types.reshape(-1, 1)),
-                    components=[],
-                    properties=Labels(
-                        ["center_type"], self.atomic_types.reshape(-1, 1)
+                    samples=Labels(
+                        ["first_atom_type", "second_atom_type"],
+                        torch.cartesian_prod(self.atomic_types, self.atomic_types),
                     ),
+                    components=[],
+                    properties=block.properties,
                 )
-                for _ in layout
+                for block in layout
             ],
         )
         self.XTY[target_name] = TensorMap(
@@ -230,6 +231,7 @@ class BaseCompositionModel(torch.nn.Module):
         self,
         systems: List[System],
         targets: Dict[str, TensorMap],
+        extra_data: Optional[Dict[str, TensorMap]] = None,
     ) -> None:
         """
         Takes a batch of systems and targets, and for each target accumulates the
@@ -239,6 +241,9 @@ class BaseCompositionModel(torch.nn.Module):
         :param targets: Dict of target names to :py:class:`TensorMap` containing
             the target values for each system in the batch.
         """
+
+        if extra_data is None:
+            extra_data = {}
 
         device = systems[0].positions.device
         dtype = systems[0].positions.dtype
@@ -255,13 +260,17 @@ class BaseCompositionModel(torch.nn.Module):
 
         # accumulate
         for target_name, target in targets.items():
+            mask = None
+
+            if target_name + "_mask" in extra_data:
+                mask = extra_data[target_name + "_mask"]
+
             for key, block in target.items():
                 if not _include_key(key):
                     continue
 
                 # Get the target block values
                 Y = block.values
-                mask = ~torch.isnan(Y).any(dim=tuple(range(1, Y.ndim)))
 
                 if self.sample_kinds[target_name] == "per_structure":
                     X = self._compute_X_per_structure(systems)
@@ -276,14 +285,52 @@ class BaseCompositionModel(torch.nn.Module):
                     )
                 X = X.to(device=device, dtype=dtype)
 
-                # Compute "XTX", i.e. X.T @ X
-                # TODO: store XTX by sample kind instead, saving memory
-                self.XTX[target_name][key].values[:] += X[mask].T @ X[mask]
+                if mask is None:
+                    # Compute "XTX", i.e. X.T @ X
+                    # TODO: store XTX by sample kind instead, saving memory
+                    self.XTX[target_name][key].values[:] += (
+                        (X.T @ X)
+                        .reshape(-1, 1)
+                        .repeat_interleave(
+                            len(self.XTX[target_name][key].properties),
+                            1,
+                        )
+                    )
 
-                # Compute "XTY", i.e. X.T @ Y
-                self.XTY[target_name][key].values[:] += torch.tensordot(
-                    X[mask], Y[mask], dims=([0], [0])
-                )
+                    # Compute "XTY", i.e. X.T @ Y
+                    self.XTY[target_name][key].values[:] += torch.tensordot(
+                        X, Y, dims=([0], [0])
+                    )
+
+                else:
+                    # If there is a mask, we need to compute X separately for each
+                    # property, slicing to keep only atomic samples that are not masked
+                    # for that property.
+
+                    mask_block = mask[key]
+
+                    for property_i in range(len(block.properties)):
+                        # Build a boolean mask for the samples that are not masked for
+                        # this property
+                        mask_vals_property = mask_block.values[..., property_i]
+                        sample_mask = torch.all(
+                            mask_vals_property == 1.0,
+                            dim=tuple(range(1, mask_vals_property.ndim)),
+                        )
+
+                        # Compute "XTX", i.e. X.T @ X
+                        self.XTX[target_name][key].values[:, property_i] += (
+                            X[sample_mask].T @ X[sample_mask]
+                        ).reshape(-1)
+
+                        # Compute "XTY", i.e. X.T @ Y
+                        self.XTY[target_name][key].values[..., property_i] += (
+                            torch.tensordot(
+                                X[sample_mask],
+                                Y[sample_mask, ..., property_i],
+                                dims=([0], [0]),
+                            )
+                        )
 
     def fit(
         self,
@@ -313,32 +360,45 @@ class BaseCompositionModel(torch.nn.Module):
             for key in self.XTX[target_name].keys:
                 XTX_block = self.XTX[target_name][key]
                 XTY_block = self.XTY[target_name][key]
+                weight_vals = torch.zeros_like(self.weights[target_name][key].values)
 
-                XTX_values = XTX_block.values
-                XTY_values = XTY_block.values
+                for property_i in range(len(self.XTY[target_name][key].properties)):
+                    XTX_values = XTX_block.values.reshape(
+                        len(self.atomic_types), len(self.atomic_types), -1
+                    )[..., property_i]
+                    XTY_values = XTY_block.values[..., property_i]
 
-                if target_name in fixed_weights:
-                    weight_vals = torch.vstack(
-                        [
-                            torch.full(
-                                (
-                                    1,
-                                    *[len(c) for c in XTY_block.components],
-                                    len(XTY_block.properties),
-                                ),
-                                fixed_weights[target_name][int(atomic_type)],
-                                dtype=XTY_values.dtype,
-                                device=XTY_values.device,
-                            )
-                            for atomic_type in self.atomic_types
-                        ]
+                    if target_name in fixed_weights:
+                        weight_vals[..., property_i] = torch.vstack(
+                            [
+                                torch.full(
+                                    (
+                                        1,
+                                        *[len(c) for c in XTY_block.components],
+                                        1,
+                                    ),
+                                    fixed_weights[target_name][int(atomic_type)],
+                                    dtype=XTY_values.dtype,
+                                    device=XTY_values.device,
+                                )
+                                for atomic_type in self.atomic_types
+                            ]
+                        )
+                    else:
+                        XTY_shape = XTY_values.shape
+                        if len(XTY_values.shape) != 2:
+                            XTY_values = XTY_values.reshape(XTY_values.shape[0], -1)
+                        weight_vals[..., property_i] = _solve_linear_system(
+                            XTX_values, XTY_values
+                        ).reshape(*XTY_shape)
+
+                if torch.isnan(weight_vals).any():
+                    raise ValueError(
+                        f"composition weights for target {target_name} and key"
+                        f" {key} contain NaNs. This may be due to accumulation"
+                        " of NaN-padded target data - please ensure that a mask"
+                        " if being provided during accumulation."
                     )
-                else:
-                    XTY_shape = XTY_values.shape
-                    if len(XTY_values.shape) != 2:
-                        XTY_values = XTY_values.reshape(XTY_values.shape[0], -1)
-                    weight_vals = _solve_linear_system(XTX_values, XTY_values)
-                    weight_vals = weight_vals.reshape(*XTY_shape)
 
                 blocks.append(
                     TensorBlock(

src/metatrain/utils/additive/composition.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional, Union
 import metatensor.torch as mts
 import torch
 from metatensor.torch import Labels, TensorBlock, TensorMap
-from metatomic.torch import ModelOutput, NeighborListOptions, System
+from metatomic.torch import ModelOutput, System
 from torch.utils.data import DataLoader, DistributedSampler
 
 from metatrain.utils.data import (
@@ -12,7 +12,6 @@ from metatrain.utils.data import (
     CombinedDataLoader,
     Dataset,
 )
-from metatrain.utils.neighbor_lists import get_system_with_neighbor_lists_transform
 
 from ..data import DatasetInfo, TargetInfo, unpack_batch
 from ..jsonschema import validate
@@ -92,7 +91,7 @@ class CompositionModel(torch.nn.Module):
     def _get_dataloader(
         self,
         datasets: List[Union[Dataset, torch.utils.data.Subset]],
-        requested_neighbor_lists: List[NeighborListOptions],
+        collate_fn: CollateFn,
         batch_size: int,
         is_distributed: bool,
     ) -> DataLoader:
@@ -112,17 +111,6 @@ class CompositionModel(torch.nn.Module):
         :param is_distributed: Whether to use distributed sampling for the dataloader.
         :return: A DataLoader for the CompositionModel fitting.
         """
-        # Create the collate function
-        collate_fn = CollateFn(
-            target_keys=list(self.dataset_info.targets.keys()),
-            callables=[
-                # these neighbor lists might be required by the other additive models
-                # that need to be removed from the targets before fitting the
-                # composition weights
-                get_system_with_neighbor_lists_transform(requested_neighbor_lists)
-            ],
-        )
-
         dtype = datasets[0][0]["system"].positions.dtype
         if dtype != torch.float64:
             raise ValueError(
@@ -173,6 +161,7 @@ class CompositionModel(torch.nn.Module):
         self,
         datasets: List[Union[Dataset, torch.utils.data.Subset]],
         additive_models: List[torch.nn.Module],
+        collate_fn: CollateFn,
         batch_size: int,
         is_distributed: bool,
         fixed_weights: Optional[Dict[str, Dict[int, float]]] = None,
@@ -191,6 +180,7 @@ class CompositionModel(torch.nn.Module):
         :param datasets: A list of datasets to use for training.
         :param additive_models: A list of additive models whose contributions will be
             removed from the targets before training.
+        :param collate_fn: Collate function to use for the dataloader.
         :param batch_size: The batch size to use for training.
         :param is_distributed: Whether to use distributed sampling for the dataloader.
         :param fixed_weights: A dictionary specifying which targets should be treated as
@@ -205,17 +195,9 @@ class CompositionModel(torch.nn.Module):
         if len(self.target_infos) == 0:  # no (new) targets to fit
             return
 
-        # Create dataloader for the training datasets. Note that these might need
-        # neighbor lists if any of the `additive_models` require them.
-        requested_neighbor_lists = []
-        for additive_model in additive_models:
-            if hasattr(additive_model, "requested_neighbor_lists"):
-                requested_neighbor_lists += additive_model.requested_neighbor_lists()
+        # Create dataloader for the training datasets
         dataloader = self._get_dataloader(
-            datasets,
-            requested_neighbor_lists,
-            batch_size,
-            is_distributed=is_distributed,
+            datasets, collate_fn, batch_size, is_distributed=is_distributed
         )
 
         if fixed_weights is None:
@@ -225,8 +207,10 @@ class CompositionModel(torch.nn.Module):
 
         # accumulate
         for batch in dataloader:
-            systems, targets, _ = unpack_batch(batch)
-            systems, targets, _ = batch_to(systems, targets, device=device)
+            systems, targets, extra_data = unpack_batch(batch)
+            systems, targets, extra_data = batch_to(
+                systems, targets, extra_data, device=device
+            )
             # only accumulate the targets that do not use fixed weights
             targets = {
                 target_name: targets[target_name]
@@ -247,7 +231,7 @@ class CompositionModel(torch.nn.Module):
                         for target_name in targets
                     },
                 )
-            self.model.accumulate(systems, targets)
+            self.model.accumulate(systems, targets, extra_data)
 
         if is_distributed:
             torch.distributed.barrier()

src/metatrain/utils/scaler/_base_scaler.py
@@ -6,9 +6,8 @@ The class ``Scaler`` wraps this to be compatible with metatrain-style objects.
 import logging
 from typing import Dict, List, Optional, Union
 
-import metatensor.torch as mts
 import torch
-from metatensor.torch import Labels, LabelsEntry, TensorBlock, TensorMap
+from metatensor.torch import Labels, TensorBlock, TensorMap
 from metatomic.torch import System
 
 
@@ -35,6 +34,7 @@ class BaseScaler(torch.nn.Module):
     scales: Dict[str, TensorMap]
     sample_kinds: Dict[str, str]
     type_to_index: torch.Tensor
+    pair_type_to_index: torch.Tensor
     N: Dict[str, TensorMap]
     Y2: Dict[str, TensorMap]
 
@@ -54,6 +54,17 @@ class BaseScaler(torch.nn.Module):
         )
         for i, atomic_type in enumerate(self.atomic_types):
             self.type_to_index[atomic_type] = i
+
+        # go from a pair of atomic types to their position in `self.atomic_types`
+        self.register_buffer(
+            "pair_type_to_index",
+            torch.empty(
+                max(self.atomic_types) + 1, max(self.atomic_types) + 1, dtype=torch.long
+            ),
+        )
+        for i, first_atom_type in enumerate(self.atomic_types):
+            for j, second_atom_type in enumerate(self.atomic_types):
+                self.pair_type_to_index[first_atom_type, second_atom_type] = i + j
 
         # Add targets based on provided layouts
         for target_name, layout in layouts.items():
@@ -99,7 +110,11 @@ class BaseScaler(torch.nn.Module):
         elif layout.sample_names == valid_sample_names[2]:
             self.sample_kinds[target_name] = "per_pair"
             samples = Labels(
-                ["atomic_type"], torch.arange(len(self.atomic_types)).reshape(-1, 1)
+                ["first_atom_type", "second_atom_type"],
+                torch.cartesian_prod(
+                    torch.arange(len(self.atomic_types)),
+                    torch.arange(len(self.atomic_types)),
+                ),
             )
 
         else:
@@ -108,14 +123,6 @@ class BaseScaler(torch.nn.Module):
                 f" {layout.sample_names} but expected one of "
                 f"{valid_sample_names}."
             )
-
-        layout = mts.filter_blocks(
-            layout,
-            Labels(
-                layout.keys.names,
-                torch.vstack([key.values for key in layout.keys if _include_key(key)]),
-            ),
-        )
 
         # Initialize TensorMaps for the quantities to accumulate for this target.
         self.N[target_name] = TensorMap(
@@ -199,9 +206,6 @@ class BaseScaler(torch.nn.Module):
                 mask = extra_data[target_name + "_mask"]
 
             for key, block in target.items():
-                if not _include_key(key):
-                    continue
-
                 if self.sample_kinds[target_name] == "per_structure":
                     if mask is not None:
                         raise NotImplementedError(
@@ -225,12 +229,7 @@ class BaseScaler(torch.nn.Module):
                     self.N[target_name][key].values[0] += N
                     self.Y2[target_name][key].values[0] += Y2_values
 
-                else:
-                    assert self.sample_kinds[target_name] in ["per_atom", "per_pair"]
-
-                    if "n_centers" in key.names:
-                        assert key["n_centers"] == 1
-
+                elif self.sample_kinds[target_name] == "per_atom":
                     Y_block = block.to(device=device, dtype=dtype)
 
                     # Here it is assumed that the samples of the block correspond to the
@@ -262,13 +261,67 @@ class BaseScaler(torch.nn.Module):
                         # Compute the Y2 values and sum over samples and components
                         Y2_values = torch.sum(Y**2, dim=list(range(0, Y.dim() - 1)))
 
-                        # Repeat the along the component axes (if any) and accumulate
+                        # Repeat the along the component axes (if any) and
+                        # accumulate
                         self.N[target_name][key].values[
                             self.type_to_index[atomic_type]
                         ] += N
                         self.Y2[target_name][key].values[
                             self.type_to_index[atomic_type]
                         ] += Y2_values
+
+                elif self.sample_kinds[target_name] == "per_pair":
+                    Y_block = block.to(device=device, dtype=dtype)
+
+                    # Get the types of the atoms. TODO: quicker way of doing this?
+                    Y_block_types = torch.tensor(
+                        [
+                            [systems[A].types[i], systems[A].types[j]]
+                            for A, i, j in Y_block.samples.values[:, :3]
+                        ]
+                    )
+
+                    for first_atom_type in self.atomic_types:
+                        for second_atom_type in self.atomic_types:
+                            # Slice the block to only include samples of the current atomic
+                            # type
+                            samples_type_mask = (
+                                Y_block_types[:, 0] == first_atom_type
+                            ) & (Y_block_types[:, 1] == second_atom_type)
+                            Y = Y_block.values[samples_type_mask]
+
+                            # Compute the number of samples and components in this block,
+                            # account for the mask if available
+                            if mask is None:
+                                N = Y.numel() // Y.shape[-1]
+                            else:
+                                # For each property, count N as the number of samples *
+                                # components where the mask is True.
+                                mask_values = mask.block(key).values[samples_type_mask]
+                                assert Y.shape == mask_values.shape
+                                N = torch.sum(
+                                    mask_values, dim=list(range(0, Y.dim() - 1))
+                                )
+
+                                # To ensure that Y^2 isn't accumulated on masked entries,
+                                # mutliply Y by the mask (either 1 for real data or 0 for
+                                # padded data).
+                                Y = Y * mask_values.to(Y.dtype)
+
+                            # Compute the Y2 values and sum over samples and components
+                            Y2_values = torch.sum(Y**2, dim=list(range(0, Y.dim() - 1)))
+
+                            # Repeat the along the component axes (if any) and accumulate
+                            self.N[target_name][key].values[
+                                self.pair_type_to_index[
+                                    first_atom_type, second_atom_type
+                                ]
+                            ] += N
+                            self.Y2[target_name][key].values[
+                                self.pair_type_to_index[
+                                    first_atom_type, second_atom_type
+                                ]
+                            ] += Y2_values
 
     def fit(
         self,
@@ -330,26 +383,9 @@ class BaseScaler(torch.nn.Module):
                         Y2_values_type / N_values_type
                     )  # (do not use Bessel's correction)
 
-                    # Provide a warning for scales that cannot be computed. These will
-                    # be NaN as N_values_type for this property will be zero.
-                    if torch.isnan(
-                        scale_vals_type
-                    ).any():  # this can only happen for per-atom targets
-                        assert self.sample_kinds[target_name] in [
-                            "per_atom",
-                            "per_pair",
-                        ]
-                        logging.info(
-                            f"Per-atom target {target_name} has not enough samples in "
-                            f"block {key} for atomic type"
-                            f"{self.atomic_types[type_index]} to compute statistics. "
-                            "The scales of one or more property cannot be computed."
-                        )
-                        scale_vals_type[torch.isnan(scale_vals_type)] = 1.0
-
-                    # If any scales are zero, set them to 1.0
-                    if torch.any(scale_vals_type == 0):
-                        scale_vals_type[scale_vals_type == 0] = 1.0
+                    # If any scales are zero or NaN, set them to 1.0
+                    scale_vals_type[scale_vals_type == 0] = 1.0
+                    scale_vals_type[torch.isnan(scale_vals_type)] = 1.0
 
                     scale_vals_type = scale_vals_type.contiguous()
                     block.values[type_index][:] = scale_vals_type
@@ -398,13 +434,6 @@ class BaseScaler(torch.nn.Module):
 
             prediction_blocks: List[TensorBlock] = []
             for key, output_block in output_tmap.items():
-                # TODO: Use the unscaled offsite blocks for now
-                if key not in self.scales[output_name].keys:
-                    # sanity check for now. TODO: handle offsite scaling
-                    assert "n_centers" in key.names and key["n_centers"] == 2
-                    prediction_blocks.append(output_block)
-                    continue
-
                 # Find the scales block and check metadata
                 scales_block = self.scales[output_name].block(key)
                 assert scales_block.properties == output_block.properties, (
@@ -414,7 +443,6 @@ class BaseScaler(torch.nn.Module):
                 )
 
                 # Scale each atomic type separately
-                output_block_types = torch.cat([system.types for system in systems])
                 scaled_vals = output_block.values
 
                 # unsqueeze scales_block.values to make broadcasting work
@@ -464,8 +492,8 @@ class BaseScaler(torch.nn.Module):
                                 ),
                             )
 
-                else:
-                    assert self.sample_kinds[output_name] in ["per_atom", "per_pair"]
+                elif self.sample_kinds[output_name] == "per_atom":
+                    output_block_types = torch.cat([system.types for system in systems])
 
                     # TODO: gradients of per-atom targets are not supported
                     if len(output_block.gradients_list()) > 0:
@@ -496,6 +524,46 @@ class BaseScaler(torch.nn.Module):
                         components=output_block.components,
                         properties=output_block.properties,
                     )
+
+                else:
+                    assert self.sample_kinds[output_name] == "per_pair"
+
+                    unique_system_ids, inverse_indices = torch.unique(
+                        output_block.samples.values[:, 0], return_inverse=True
+                    )
+                    sample_idxs = []
+                    for system_i, system in enumerate(systems):
+                        i_idxs = output_block.samples.values[
+                            inverse_indices == system_i, 1
+                        ]
+                        j_idxs = output_block.samples.values[
+                            inverse_indices == system_i, 2
+                        ]
+                        Zi_idxs = system.types[i_idxs]
+                        Zj_idxs = system.types[j_idxs]
+                        sample_idxs.append(self.pair_type_to_index[Zi_idxs, Zj_idxs])
+                    sample_idxs = torch.cat(sample_idxs)
+
+                    # TODO: gradients of per-atom targets are not supported
+                    if len(output_block.gradients_list()) > 0:
+                        raise NotImplementedError(
+                            "scaling of gradients is not implemented for per-atom "
+                            "targets"
+                        )
+
+                    # Scale the values of the output block
+                    if remove:  # remove the scaler
+                        scaled_vals = scaled_vals / scales_block_values[sample_idxs]
+                    else:  # apply the scaler
+                        scaled_vals = scaled_vals * scales_block_values[sample_idxs]
+
+                    prediction_block = TensorBlock(
+                        values=scaled_vals,
+                        samples=output_block.samples,
+                        components=output_block.components,
+                        properties=output_block.properties,
+                    )
+
                 prediction_blocks.append(prediction_block)
 
             predictions[output_name] = TensorMap(
@@ -584,42 +652,3 @@ class BaseScaler(torch.nn.Module):
             target_name: tm.to(device=device, dtype=dtype)
             for target_name, tm in self.scales.items()
         }
-
-
-def _include_key(key: LabelsEntry) -> bool:
-    """
-    Determines whether a block indexed by the input ``key`` should be included in the
-    scaler model. All keys except those for offsite blocks of per-pair targets are
-    included.
-
-    :param key: The key to check.
-
-    :return: Whether the key should be included in the composition model.
-    """
-    valid_key_names = [
-        ["_"],  # scalar
-        ["o3_lambda", "o3_sigma"],  # spherical
-        ["o3_lambda", "o3_sigma", "n_centers"],  # spherical per-atom
-    ]
-    include_key = False
-
-    if key.names == valid_key_names[0]:
-        include_key = True
-
-    elif key.names == valid_key_names[1]:
-        if key["o3_lambda"] == 0 and key["o3_sigma"] == 1:
-            include_key = True
-
-    elif key.names == valid_key_names[2]:
-        if key["n_centers"] == 1:
-            include_key = True
-        else:
-            assert key["n_centers"] == 2
-            include_key = False
-
-    else:
-        raise ValueError(
-            f"key names {key.names} not in valid key names {valid_key_names}"
-        )
-
-    return include_key

src/metatrain/utils/scaler/scaler.py
@@ -13,6 +13,7 @@ from metatrain.utils.data import (
 )
 from metatrain.utils.per_atom import average_by_num_atoms
 
+from ..additive import remove_additive
 from ..data import (
     DatasetInfo,
     TargetInfo,
@@ -20,7 +21,7 @@ from ..data import (
 )
 from ..jsonschema import validate
 from ..transfer import batch_to
-from ._base_scaler import BaseScaler, _include_key
+from ._base_scaler import BaseScaler
 
 
 class Scaler(torch.nn.Module):
@@ -137,6 +138,7 @@ class Scaler(torch.nn.Module):
     def train_model(
         self,
         datasets: List[Union[Dataset, torch.utils.data.Subset]],
+        additive_models: List[torch.nn.Module],
         collate_fn: CollateFn,
         batch_size: int,
         is_distributed: bool,
@@ -178,6 +180,17 @@ class Scaler(torch.nn.Module):
             if len(targets) == 0:
                 break
 
+            # remove additive contributions from these targets
+            for additive_model in additive_models:
+                targets = remove_additive(
+                    systems,
+                    targets,
+                    additive_model,
+                    {
+                        target_name: self.target_infos[target_name]
+                        for target_name in targets
+                    },
+                )
             targets = average_by_num_atoms(targets, systems, [])
             self.model.accumulate(systems, targets, extra_data)
 
@@ -299,7 +312,11 @@ class Scaler(torch.nn.Module):
 
         elif layout.sample_names == valid_sample_names[2]:
             samples = Labels(
-                ["atomic_type"], torch.arange(len(self.atomic_types)).reshape(-1, 1)
+                ["first_atom_type", "second_atom_type"],
+                torch.cartesian_prod(
+                    torch.arange(len(self.atomic_types)),
+                    torch.arange(len(self.atomic_types)),
+                ),
             )
 
         else:
@@ -308,14 +325,6 @@ class Scaler(torch.nn.Module):
                 f" {layout.sample_names} but expected one of "
                 f"{valid_sample_names}."
             )
-
-        layout = mts.filter_blocks(
-            layout,
-            Labels(
-                layout.keys.names,
-                torch.vstack([key.values for key in layout.keys if _include_key(key)]),
-            ),
-        )
 
         fake_scales = TensorMap(
             keys=layout.keys,