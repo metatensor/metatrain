b6d46d3128f0a7f0f7a3a9a0d420a443d635a553
Pol <pfebrer96@gmail.com>
2025-11-10 22:52:55 +0100

    fixed tests

--- src/metatrain/utils/pydantic.py
+++ src/metatrain/utils/pydantic.py
@@ -1,8 +1,90 @@
-from pydantic import TypeAdapter, create_model
+from typing import Any
+
+from pydantic import BaseModel, TypeAdapter, ValidationError, create_model
 
 from ..share.base_hypers import BaseHypers
 from .abc import ModelInterface, TrainerInterface
 from .hypers import get_hypers_cls
+
+
+class MetatrainValidationError(Exception):
+    """This class transforms Pydantic validation errors into a
+    more user-friendly format.
+
+    :param model: The Pydantic model class or TypedDict that was
+        used for validation.
+    :param errors: The list of Pydantic error dictionaries.
+    """
+
+    def __init__(self, model: Any, errors: list[dict]):
+        self.model = model
+        self.errors = errors
+
+    def get_error_string(self, error: dict) -> str:
+        """Given an individual error from Pydantic, return a user-friendly string.
+
+        :param error: The Pydantic error dictionary.
+
+        :return: The formatted error string to display to the user.
+        """
+
+        # This is a field that was not expected
+        if error["type"] == "extra_forbidden":
+            extra_field = {".".join(error["loc"])}
+            return f"Unrecognized option '{extra_field}'."
+
+        # If it doesn't match any special case, use the default Pydantic formatting
+        return self.default_pydantic(error)
+
+    def default_pydantic(self, err: dict) -> str:
+        """Default Pydantic error formatting.
+
+        :param err: The Pydantic error dictionary.
+        :return: The formatted error string to display to the user.
+        """
+        pydantic_error = f"{err['msg']}"
+        pydantic_error += f" [type={err['type']}, input_value={err['input']},"
+        pydantic_error += f" input_type={err.get('type', 'unknown')}]"
+
+        pydantic_error += f"\n\tFor further information visit {err['url']}"
+        return pydantic_error
+
+    def __str__(self) -> str:
+        """Return a string representation of all validation errors.
+
+        :return: The formatted error string to display to the user.
+        """
+        error_str = f"{len(self.errors)} validation errors occurred:\n"
+        for i, err in enumerate(self.errors):
+            error_str += (
+                f"[Error {i}] {'.'.join(err['loc'])}\n\t{self.get_error_string(err)}\n"
+            )
+        return error_str
+
+
+def validate(model_cls: Any, data: dict, **kwargs: Any) -> None:
+    """Validate with pydantic, raising custom metatrain errors.
+
+    :param model_cls: The Pydantic model class to use for validation.
+      If it is not a pydantic model, it will be adapted to pydantic
+      using ``pydantic.TypeAdapter``.
+    :param data: The data to validate.
+    :param **kwargs: Additional keyword arguments to pass to the validation method.
+
+    :raises MetatrainValidationError: If validation fails.
+    """
+
+    if issubclass(model_cls, BaseModel):
+        try:
+            model_cls.model_validate(data, **kwargs)
+        except ValidationError as e:
+            raise MetatrainValidationError(model_cls, e.errors()) from e
+    else:
+        adapter = TypeAdapter(model_cls)
+        try:
+            adapter.validate_python(data, **kwargs)
+        except ValidationError as e:
+            raise MetatrainValidationError(model_cls, e.errors()) from e
 
 
 def validate_architecture_options(
@@ -13,8 +95,6 @@ def validate_architecture_options(
     :param options: The architecture options to validate.
     :param model: The model class of the architecture.
     :param trainer: The trainer class of the architecture.
-
-    :raises ValueError: If the architecture name is unknown.
     """
     model_hypers = get_hypers_cls(model)
     trainer_hypers = get_hypers_cls(trainer)
@@ -38,7 +118,7 @@ def validate_architecture_options(
         options["atomic_types"] = []
         added_atomic_types = True
 
-    ArchitectureOptions.model_validate(options)
+    validate(ArchitectureOptions, options)
 
     if added_atomic_types:
         del options["atomic_types"]
@@ -51,4 +131,4 @@ def validate_base_options(options: dict) -> None:
 
     :raises ValueError: If the options are invalid.
     """
-    TypeAdapter(BaseHypers).validate_python(options)
+    validate(BaseHypers, options)
