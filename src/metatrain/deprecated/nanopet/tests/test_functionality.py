7c1bbf04b0165df6b6e613245583afb9352e5334
Pol <pfebrer96@gmail.com>
2025-11-10 21:51:29 +0100

    Test hypers in architecture tests

--- src/metatrain/deprecated/nanopet/tests/test_functionality.py
+++ src/metatrain/deprecated/nanopet/tests/test_functionality.py
@@ -20,6 +20,14 @@ from metatrain.utils.pydantic import MetatrainValidationError
 from . import DEFAULT_HYPERS, MODEL_HYPERS
 
 
+def test_valid_defaults():
+    """Tests that the default hypers pass the architecture options check."""
+    hypers = OmegaConf.create(DEFAULT_HYPERS)
+    check_architecture_options(
+        name="deprecated.nanopet", options=OmegaConf.to_container(hypers)
+    )
+
+
 def test_nanopet_padding():
     """Tests that the model predicts the same energy independently of the
     padding size."""
