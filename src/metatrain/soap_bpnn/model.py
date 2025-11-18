85bb06f079279b53aebf6a7a8709f290da7910c3
Pol <pfebrer96@gmail.com>
2025-11-18 11:45:29 +0100

    Make things right

--- src/metatrain/soap_bpnn/model.py
+++ src/metatrain/soap_bpnn/model.py
@@ -197,8 +197,6 @@ class SoapBpnn(ModelInterface[ModelHypers]):
             ],
         }
     )
-    __hypers_cls__ = ModelHypers
-
     component_labels: Dict[str, List[List[Labels]]]  # torchscript needs this
     cartesian_targets: List[str]  # torchscript needs this
 
