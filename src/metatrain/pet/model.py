9614093414682c694aa2b1f822f1aa7ad4041c4a
Filippo Bigi <98903385+frostedoyster@users.noreply.github.com>
2025-11-20 21:15:56 +0100

    Upgrade PET checkpoints for `inherit_heads` fine-tuning option (#902)

--- src/metatrain/pet/model.py
+++ src/metatrain/pet/model.py
@@ -49,7 +49,7 @@ class PET(ModelInterface[ModelHypers]):
         targets.
     """
 
-    __checkpoint_version__ = 8
+    __checkpoint_version__ = 9
     __supported_devices__ = ["cuda", "cpu"]
     __supported_dtypes__ = [torch.float32, torch.float64]
     __default_metadata__ = ModelMetadata(
