24ae8f5d3c4fb4ea139ff0752f8e364b97b55f24
MarkusFas <MarkusFas7@hotmail.com>
2025-11-24 15:33:06 +0100

    add stderr print

--- examples/0-beginner/02-fine-tuning.py
+++ examples/0-beginner/02-fine-tuning.py
@@ -146,7 +146,10 @@ from metatomic.torch.ase_calculator import MetatomicCalculator
 # Here, we run ``mtt train`` as a subprocess, and delete the old outputs folder
 # line, e.g. ``mtt train options-model.yaml -o model.pt``.
 subprocess.run(["rm", "-rf", "outputs"])
-subprocess.run(["mtt", "train", "options-ft.yaml", "-o", "model-ft.pt"], check=True)
+result = subprocess.run(["mtt", "train", "options-ft.yaml", "-o", "model-ft.pt"], check=True
+               capture_output=True, text=True)
+print("STDOUT:\n", result.stdout)
+print("STDERR:\n", result.stderr)
 csv_path = glob.glob("outputs/*/*/train.csv")[-1]
 with open(csv_path, "r") as f:
     header = f.readline().strip().split(",")
