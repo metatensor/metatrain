Fix use computation of loss in physical units

src/metatrain/pet/trainer.py
@@ -223,11 +223,21 @@ class Trainer(TrainerInterface):
             target_info_dict=train_targets, extra_data_info_dict=extra_data_info
         )
         requested_neighbor_lists = get_requested_neighbor_lists(model)
-        if model.hypers["rescale_train_predictions"]:
-            logging.info("Training with rescaled predictions (physical units)")
-            remove_scale_transform = [get_remove_scale_transform(scaler)]
+        if self.hypers["scale_targets"]:
+            if self.hypers["rescale_predictions"]:
+                logging.info("Training with rescaled predictions (physical units)")
+                # the predictions will be rescaled to have physical units, so the targets
+                # shouldn't be scaled in the collate fn (i.e. don't use the remove scale
+                # transform)
+                remove_scale_transform = []
+            else:
+                logging.info("Training with scaled predictions (scaled units)")
+                # the predictions will have scaled units, so the targets need to be scaled
+                # in the collate fn (i.e. use the remove scale transform)
+                remove_scale_transform = [get_remove_scale_transform(scaler)]
         else:
-            logging.info("Training with scaled predictions (scaled units)")
+            # no scales are computed, so they don't need to be removed from the targets
+            # in the collate function
             remove_scale_transform = []
         collate_fn_train = CollateFn(
             target_keys=list(train_targets.keys()),
@@ -399,6 +409,11 @@ class Trainer(TrainerInterface):
                     {key: train_targets[key] for key in targets.keys()},
                     is_training=True,
                 )
+                if self.hypers["scale_targets"] and self.hypers["rescale_predictions"]:
+                    # rescale the predictions to physical units for loss calculation
+                    predictions = (model.module if is_distributed else model).scaler(
+                        systems, predictions
+                    )
 
                 # average by the number of atoms
                 predictions = average_by_num_atoms(
@@ -425,21 +440,22 @@ class Trainer(TrainerInterface):
                     torch.distributed.all_reduce(train_loss_batch)
                 train_loss += train_loss_batch.item()
 
-                if (model.module if is_distributed else model).hypers["rescale_train_predictions"]:
-                    # the loss was computed in physical units with the predictions and
-                    # targets already rescaled. No rescaling needs to happen here for
-                    # reporting metrics
-                    scaled_predictions = predictions
-                    scaled_targets = targets
-                else:
-                    # the loss was computed in scaled units. Rescaling needs to happen here for
-                    # reporting metrics in physical units
+                if self.hypers["scale_targets"] and not self.hypers["rescale_predictions"]:
+                    # if the targets have been scaled and the predictions are in scaled
+                    # units, scale both predictions and targets up to physical units for
+                    # calculation of metrics
                     scaled_predictions = (model.module if is_distributed else model).scaler(
                         systems, predictions
                     )
                     scaled_targets = (model.module if is_distributed else model).scaler(
                         systems, targets
                     )
+                else:
+                    # both the targets and predictions are already in physical units,
+                    # ready for calculation of metrics
+                    scaled_predictions = predictions
+                    scaled_targets = targets
+
                 train_rmse_calculator.update(
                     scaled_predictions, scaled_targets, extra_data
                 )
@@ -474,6 +490,11 @@ class Trainer(TrainerInterface):
                     {key: train_targets[key] for key in targets.keys()},
                     is_training=False,
                 )
+                if self.hypers["scale_targets"] and self.hypers["rescale_predictions"]:
+                    # rescale the predictions to physical units for loss calculation
+                    predictions = (model.module if is_distributed else model).scaler(
+                        systems, predictions
+                    )
 
                 # average by the number of atoms
                 predictions = average_by_num_atoms(
@@ -487,21 +508,22 @@ class Trainer(TrainerInterface):
                     torch.distributed.all_reduce(val_loss_batch)
                 val_loss += val_loss_batch.item()
 
-                if (model.module if is_distributed else model).hypers["rescale_train_predictions"]:
-                    # the loss was computed in physical units with the predictions and
-                    # targets already rescaled. No rescaling needs to happen here for
-                    # reporting metrics
-                    scaled_predictions = predictions
-                    scaled_targets = targets
-                else:
-                    # the loss was computed in scaled units. Rescaling needs to happen here for
-                    # reporting metrics in physical units
+                if self.hypers["scale_targets"] and not self.hypers["rescale_predictions"]:
+                    # if the targets have been scaled and the predictions are in scaled
+                    # units, scale both predictions and targets up to physical units for
+                    # calculation of metrics
                     scaled_predictions = (model.module if is_distributed else model).scaler(
                         systems, predictions
                     )
                     scaled_targets = (model.module if is_distributed else model).scaler(
                         systems, targets
                     )
+                else:
+                    # both the targets and predictions are already in physical units,
+                    # ready for calculation of metrics
+                    scaled_predictions = predictions
+                    scaled_targets = targets
+
                 val_rmse_calculator.update(
                     scaled_predictions, scaled_targets, extra_data
                 )