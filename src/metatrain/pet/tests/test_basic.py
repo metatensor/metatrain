7b1d2b8208fa71be05f6b16e1d8b73caf9023805
Joseph Abbott <joseph.william.abbott@gmail.com>
2025-12-16 11:25:14 +0100

    Reduce size of the checkpoint

--- src/metatrain/pet/tests/test_basic.py
+++ src/metatrain/pet/tests/test_basic.py
@@ -23,6 +23,7 @@ class PETTests(ArchitectureTests):
         hypers = get_default_hypers(self.architecture)["model"]
         hypers = copy.deepcopy(hypers)
         hypers["d_pet"] = 1
+        hypers["d_node"] = 1
         hypers["d_head"] = 1
         hypers["d_feedforward"] = 1
         hypers["num_heads"] = 1
