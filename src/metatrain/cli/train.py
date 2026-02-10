0917bea32d97f92d5dc3845b8eedd6decfb37730
Paolo Pegolo <paolo.pegolo@epfl.ch>
2026-02-10 14:51:25 +0100

    Skip model evaluation after LLPR calibration or training (#1041)

--- src/metatrain/cli/train.py
+++ src/metatrain/cli/train.py
@@ -607,6 +607,13 @@ def train_model(
     # EVALUATE FINAL MODEL ####
     ###########################
 
+    # TODO: possibly control this better if and when wrappers archs will be treated
+    # differently in the future
+    if architecture_name == "llpr":
+        if not hypers["training"]["train_all_parameters"]:
+            # Skip final evaluation for LLPR
+            return
+
     mts_atomistic_model = load_model(
         path=output,
         extensions_directory=extensions if _has_extensions() else None,
