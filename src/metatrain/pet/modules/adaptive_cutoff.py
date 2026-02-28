edb7282c59c54fea62c9da2e5dc79b821f489ae6
Filippo Bigi <98903385+frostedoyster@users.noreply.github.com>
2026-02-28 14:21:57 +0100

    Update src/metatrain/pet/modules/adaptive_cutoff.py

--- src/metatrain/pet/modules/adaptive_cutoff.py
+++ src/metatrain/pet/modules/adaptive_cutoff.py
@@ -126,7 +126,7 @@ def get_gaussian_cutoff_weights(
     """
     # this early out prevents aggregation (taking the max) over empty lists
     if effective_num_neighbors.numel() == 0:
-        return torch.zeros_like(effective_num_neighbors)
+        return torch.empty_like(effective_num_neighbors)
 
     diff = effective_num_neighbors - num_neighbors_adaptive
 
