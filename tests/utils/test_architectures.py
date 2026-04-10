78f6d0be723c104ad42b2bf9854d25d14ccd62d6
Johannes Spies <13813209+johannes-spies@users.noreply.github.com>
2026-04-10 11:34:33 +0000

    increase number of architectures

--- tests/utils/test_architectures.py
+++ tests/utils/test_architectures.py
@@ -25,7 +25,7 @@ def is_None(*args, **kwargs) -> None:
 def test_find_all_architectures():
     all_arches = find_all_architectures()
 
-    assert len(all_arches) == 9
+    assert len(all_arches) == 10
 
     assert "gap" in all_arches
     assert "pet" in all_arches
