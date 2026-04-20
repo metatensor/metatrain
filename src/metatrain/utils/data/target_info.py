13da8f8ad27ed0d915f9f661823f230cfc5925c9
Pol <pfebrer96@gmail.com>
2026-04-20 10:28:43 +0200

    Initial commit

--- src/metatrain/utils/data/target_info.py
+++ src/metatrain/utils/data/target_info.py
@@ -721,7 +721,7 @@ def _get_spherical_irreps_iter(
     irreps_iter = [
         [
             (
-                irrep.get("num", 1) * target["num_subtargets"],
+                irrep.get("num", 1) * target["num_subtargets"], 
                 irrep["o3_lambda"],
                 irrep["o3_sigma"],
             )
