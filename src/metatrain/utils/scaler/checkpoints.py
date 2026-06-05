a59ca471a08bed6367d668efc19e18f0c132539e
Pol Febrer Calabozo <42074085+pfebrer@users.noreply.github.com>
2026-06-05 11:47:06 +0200

    oops mistake

--- src/metatrain/utils/scaler/checkpoints.py
+++ src/metatrain/utils/scaler/checkpoints.py
@@ -16,7 +16,7 @@ def update_per_property_scales(checkpoint: dict, scaler_key: str = "scaler") -> 
     :param scaler_key: The key under which the scaler is stored in the state_dict
       of the model.
     """
-    if checkpoint["model_data"]["hypers"]["scale_targets"]:
+    if checkpoint["train_hypers"]["scale_targets"]:
         # If the model checkpoint can output targets with multiple blocks and/or
         # multiple properties per-block, this version of metatrain cannot be used.
         # This doesn't affect MLIP checkpoints.
