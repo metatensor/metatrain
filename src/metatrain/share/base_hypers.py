0f4213f528ee0ae4c6bfa908a8a6a2de3f7d47e6
jwa7 <joseph.william.abbott@gmail.com>
2026-07-31 18:56:35 +0200

    Squashed post-hooks from Pol's branch

--- src/metatrain/share/base_hypers.py
+++ src/metatrain/share/base_hypers.py
@@ -568,3 +568,8 @@ class BaseHypers(TypedDict):
     a full dataset specification, or an ``indices`` dict referencing
     the training source file.
     """
+
+    hooks: NotRequired[dict] = {}
+    """Hooks that the model needs to apply at the end of forward to
+    produce the final outputs.
+    """
