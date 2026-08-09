04c64fa1737777a7c9447dec195713ec08843b12
GardevoirX <xqj.2001@outlook.com>
2026-08-10 00:35:39 +0200

    Allow for exporting multi-task DPA3 models

--- src/metatrain/experimental/dpa3/checkpoints.py
+++ src/metatrain/experimental/dpa3/checkpoints.py
@@ -15,6 +15,19 @@ def model_update_v1_v2(checkpoint: dict) -> None:
     update_per_property_scales(checkpoint)
 
 
+def model_update_v2_v3(checkpoint: dict) -> None:
+    """
+    Update a v2 checkpoint to v3.
+
+    :param checkpoint: The checkpoint to update.
+    """
+    # Remove the "dpa3_model" key from the hypers dictionary if it exists
+    if "dpa3_model" not in checkpoint["model_data"]["model_hypers"]:
+        checkpoint["model_data"]["model_hypers"]["dpa3_model"] = None
+    if "dpa3_model_branch" not in checkpoint["model_data"]["model_hypers"]:
+        checkpoint["model_data"]["model_hypers"]["dpa3_model_branch"] = None
+
+
 ###########################
 # TRAINER #################
 ###########################
