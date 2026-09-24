e1399763ca74fb66514c3b1ba286c308ba0ff0ec
glimmer-of-code <315626989+glimmer-of-code@users.noreply.github.com>
2026-09-23 18:32:49 -0700

    trainer checkpoint error: use consistent 'checkpoint format version' phrasing

--- src/metatrain/utils/io.py
+++ src/metatrain/utils/io.py
@@ -320,8 +320,8 @@ def trainer_from_checkpoint(
             checkpoint = architecture.__trainer__.upgrade_checkpoint(checkpoint)
         except Exception as e:
             raise RuntimeError(
-                f"{base_msg} the checkpoint is using "
-                f"version {trainer_ckpt_version}, while the current version is "
+                f"{base_msg} the checkpoint uses checkpoint format version "
+                f"{trainer_ckpt_version}, while the current version is "
                 f"{architecture.__trainer__.__checkpoint_version__}; and trying to "
                 "upgrade the checkpoint failed."
             ) from e
